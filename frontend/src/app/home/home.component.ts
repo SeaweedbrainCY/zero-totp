125cc9ff83b83b9ac998d6672be3ed68fd72b21b
seaweedbrain <nathanstchepinsky@gmail.com>
2023-07-01 18:51:12 +0200

    Update Crypt class to be compliant with front/back architecture

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -26,7 +26,6 @@ export class HomeComponent implements OnInit {
   }
 
   generateCode(){
-    console.log("generate code")
        this.duration = 30 - Math.floor(Date.now() / 1000 % 30);
       this.code=this.totp("JBSWY3DPEHPK3PXP");
   }
@@ -34,17 +33,20 @@ export class HomeComponent implements OnInit {
   
 
  login(){
-    const crypto = new Crypto(this.password);
-    crypto.encrypt(this.plaintext).then(encrypted=>{
-      this.encrypted = encrypted;
-      crypto.decrypt(this.encrypted).then(decrypted=>{
-        if(decrypted == null){
-          this.decrypted = "Wrong key"
-        } else {
-          this.decrypted = decrypted;
-        }
-       
-      })
+    const crypto = new Crypto();
+    const salt = crypto.generateRandomSalt();
+    crypto.deriveKey(salt, this.password).then(key=>{
+      crypto.encrypt(this.plaintext, key, salt).then(encrypted=>{
+        this.encrypted = encrypted;
+        crypto.decrypt(this.encrypted, key).then(decrypted=>{
+          if(decrypted == null){
+            this.decrypted = "Wrong key"
+          } else {
+            this.decrypted = decrypted;
+          }
+        
+        })
+      });
     });
     
  }
@@ -53,25 +55,30 @@ export class HomeComponent implements OnInit {
 }
 
 export class Crypto {
-  password: string;
-
-  constructor(password: string) {
-    this.password = password;
-  }
+ 
 
 
 
-   generateKeyMaterial(){
+
+
+   generateKeyMaterial(password:string){
     const enc = new TextEncoder();
-    return window.crypto.subtle.importKey("raw", enc.encode(this.password),"PBKDF2",false,["deriveBits", "deriveKey"]);
+    return window.crypto.subtle.importKey("raw", enc.encode(password),"PBKDF2",false,["deriveBits", "deriveKey"]);
   }
 
-  async deriveKey(salt:BufferSource){
-    const key_material = await this.generateKeyMaterial();
+  generateRandomSalt():string{
+    return Buffer.from(window.crypto.getRandomValues(new Uint8Array(16))).toString('base64');
+  }
+
+
+
+  async deriveKey(salt:string, password:string): Promise<CryptoKey>{
+    const key_material = await this.generateKeyMaterial(password);
+    const saltBytes = Buffer.from(salt, 'base64');
     const key = window.crypto.subtle.deriveKey(
       {
         name: "PBKDF2",
-        salt,
+        salt: saltBytes,
         iterations: 100000,
         hash: "SHA-256",
       },
@@ -83,18 +90,16 @@ export class Crypto {
     return key;
   }
 
-  async encrypt(plaintext:string) :Promise<string>{
+  async encrypt(plaintext:string, key:CryptoKey, salt:string) :Promise<string>{
     const enc = new TextEncoder();
     const plaintextBytes = enc.encode(plaintext);
     const ivBytes = window.crypto.getRandomValues(new Uint8Array(12))
-    const saltBytes = window.crypto.getRandomValues(new Uint8Array(16))
-    const key = await this.deriveKey(saltBytes);
-  
+    const saltBytes = Buffer.from(salt, 'base64');
     const encoded_encrypted = await window.crypto.subtle.encrypt({ name: "AES-GCM", iv: ivBytes }, key, plaintextBytes);
     return  Buffer.from(encoded_encrypted).toString('base64') + "," + Buffer.from(ivBytes).toString('base64') + ","+ Buffer.from(saltBytes).toString('base64');
   }
 
-  async decrypt(encrypted:string) : Promise<string | null>{
+  async decrypt(encrypted:string, key:CryptoKey) : Promise<string | null>{
     const part = encrypted.split(",")
     if (part.length != 3){
       return null;
@@ -102,17 +107,11 @@ export class Crypto {
     try{
       const cipher = Buffer.from(part[0], 'base64');
       const iv = Buffer.from(part[1], 'base64');
-      const salt = Buffer.from(part[2], 'base64');
-      const key = await this.deriveKey(salt);
       const encoded_decrypted = await window.crypto.subtle.decrypt({ name: "AES-GCM", iv: iv }, key, cipher);
       return Buffer.from(encoded_decrypted).toString("utf-8");
     } catch {
       return null
     }
-    
-
-
-
   }
   
 
