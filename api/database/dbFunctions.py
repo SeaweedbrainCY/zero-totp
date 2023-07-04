798c55dba9a5a1616ef115e6818785920e9880e6
seaweedbrain <nathanstchepinsky@gmail.com>
2023-07-04 19:57:05 +0200

    Add first table : User. Add model and basic func

---
+++ api/database/dbFunctions.py
@@ -0,0 +1,13 @@
+from database.db import db 
+from database.model import User as UserModel
+
+class User:
+
+    def getByEmail(self, email):
+        return db.session.query(UserModel).filter_by(mail=email).first()
+    
+    def create(self, username, email, password):
+        user = UserModel(username=username, mail=email, password=password)
+        db.session.add(user)
+        db.session.commit()
+        return user
--- api/database/model.py
+++ api/database/model.py
@@ -1,4 +1,8 @@
-from app import db
+from database.db import db
 
 class User(db.Model):
     __tablename__ = "User"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
+    mail = db.Column(db.String(256), nullable=False)
+    password = db.Column(db.String(256), nullable=False)
+    username = db.Column(db.String(256), nullable=False)
