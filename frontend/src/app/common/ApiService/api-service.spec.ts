8d5da471e549fb7a4da3e1336ae9170d3aebe078
seaweedbrain <nathanstchepinsky@gmail.com>
2023-07-06 20:09:30 +0200

    Add APIService class for all API related method and attributes

---
+++ frontend/src/app/common/ApiService/api-service.spec.ts
@@ -0,0 +1,7 @@
+import { ApiService } from './api-service';
+
+describe('ApiService', () => {
+  it('should create an instance', () => {
+    expect(new ApiService()).toBeTruthy();
+  });
+});
---
+++ frontend/src/app/common/ApiService/api-service.ts
@@ -0,0 +1,3 @@
+export class ApiService {
+    public static API_URL = "http://localhost:8080";
+}
