2a1e1dd0f9c628fd051c56890dac39531706c312
seaweedbrain <nathanstchepinsky@gmail.com>
2023-07-09 18:27:30 +0200

    Transfer user class in userService

--- frontend/src/app/common/User/user.service.ts
+++ frontend/src/app/common/User/user.service.ts
@@ -1,24 +1,19 @@
 import { Injectable } from '@angular/core';
-import { User } from './user';
-
+@Injectable({providedIn: 'root'}) //Note that you will need to declare it as `@Injectable`. 
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-  private user:User;
+  id:number| null =null;;
+  username:string| null =null;;
+  key:CryptoKey| null =null;;
+  derivedKeySalt:string| null =null;;
+  vault:[string:string] | null =null;
 
   constructor() {
-    this.user = new User();
    }
 
-  setUser(user:User){
-    this.user = user;
-  }
-
-  getUser():User{
-    return this.user;
-  }
 
 
 }
