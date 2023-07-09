712196ccdd5ef693fb9aaa5088786b7be03e350f
seaweedbrain <nathanstchepinsky@gmail.com>
2023-07-09 18:25:39 +0200

    Add vault and login routes

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { SignupComponent } from './signup/signup.component';
 import { LoginComponent } from './login/login.component';
+import {VaultComponent} from './vault/vault.component';
 
 const routes: Routes = [
   {path:'', component: HomeComponent},
   {path:'signup', component: SignupComponent},
-  {path:'login', component: LoginComponent}
+  {path:'login', component: LoginComponent},
+  {path:'vault', component: VaultComponent}
 ];
 
 @NgModule({
