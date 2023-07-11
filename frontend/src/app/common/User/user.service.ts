c4570eea9f0af34752b58b64bf9210caf095f632
seaweedbrain <nathanstchepinsky@gmail.com>
2023-07-11 23:05:54 +0200

    Add storage for sanitized email il local storage

--- frontend/src/app/common/User/user.service.ts
+++ frontend/src/app/common/User/user.service.ts
@@ -1,19 +1,64 @@
 import { Injectable } from '@angular/core';
+import { Utils }  from '../Utils/utils';
 @Injectable({providedIn: 'root'}) //Note that you will need to declare it as `@Injectable`. 
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-  id:number| null =null;;
-  username:string| null =null;;
-  key:CryptoKey| null =null;;
-  derivedKeySalt:string| null =null;;
-  vault:[string:string] | null =null;
+  private id:number| null =null;;
+  private email:string| null =null;;
+  private key:CryptoKey| null =null;;
+  private derivedKeySalt: string| null =null;;
+  private vault:[string:string] | null =null;
 
-  constructor() {
+  constructor(private utils: Utils) {
    }
 
+   getId() : number | null {
+    return this.id;
+   }
 
+   setId(id:number){
+    this.id = id;
+   }
+
+   getEmail() : string | null{
+    if(this.email == null){
+      this.email = localStorage.getItem("email") || null;
+    }
+    return this.utils.sanitize(this.email);;
+   }
+
+   setEmail(email:string){
+    this.email = this.utils.sanitize(email);
+    if(this.email != null){
+      localStorage.setItem("email", this.email)
+    }
+   }
+
+   getKey() : CryptoKey | null{
+    return this.key;
+   }
+
+   setKey(key:CryptoKey){
+    this.key = key;
+   }
+
+   getDerivedKeySalt(): string | null {
+    return this.derivedKeySalt;
+   }
+
+   setDerivedKeySalt(salt:string){
+    this.derivedKeySalt = salt;
+   }
+
+   getVault():[string:string] | null {
+    return this.vault;
+   }
+
+   setVault(vault:[string:string]){
+    this.vault = vault;
+   }
 
 }
