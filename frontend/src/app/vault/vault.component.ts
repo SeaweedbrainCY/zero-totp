8050f9d55c23438c6646c9b80e0507647e8b9e88
seaweedbrain <nathanstchepinsky@gmail.com>
2023-07-12 17:05:15 +0200

    Add vault design prototype

--- frontend/src/app/vault/vault.component.ts
+++ frontend/src/app/vault/vault.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../common/User/user.service';
 import { ActivatedRoute, Router } from '@angular/router';
+import { faPen, faSquarePlus } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-vault',
@@ -8,6 +9,8 @@ import { ActivatedRoute, Router } from '@angular/router';
   styleUrls: ['./vault.component.css']
 })
 export class VaultComponent implements OnInit {
+  faPen = faPen;
+  faSquarePlus = faSquarePlus;
 
   constructor(
     private userService: UserService,
@@ -17,9 +20,13 @@ export class VaultComponent implements OnInit {
 
   ngOnInit() {
     if(this.userService.getId() == null){
-      this.router.navigate(["/login/sessionKilled"], {relativeTo:this.route.root});
+      //this.router.navigate(["/login/sessionKilled"], {relativeTo:this.route.root});
     }
 
+  }
+
+  addNew(){
+    this.router.navigate(["/vault/add"], {relativeTo:this.route.root});
   }
 
 }
