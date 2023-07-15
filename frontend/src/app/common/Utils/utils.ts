994383bfd4a1866b7ae74c2cf7a29470d5758c1f
seaweedbrain <nathanstchepinsky@gmail.com>
2023-07-15 17:32:16 +0200

    Add function to convert json to ma (vault object) and the reciprocal

--- frontend/src/app/common/Utils/utils.ts
+++ frontend/src/app/common/Utils/utils.ts
@@ -16,4 +16,41 @@ export class Utils {
         }
         return unsafe_str.replaceAll('<', '&lt;').replaceAll('>', '&gt;').replaceAll('"', '&quot;').replaceAll("'", '&#039;').replaceAll('`', '&#x60;');
     }
+
+
+    vaultToJson(vault:  Map<string, Map<string, string>>):string{
+        const jsonObject: { [key: string]: { [key: string]: string } } = {};
+        for (const [key, value] of vault) {
+          jsonObject[key] = {};
+          for (const [nestedKey, nestedValue] of value) {
+            jsonObject[key][nestedKey] = nestedValue;
+          }
+        }
+
+        return JSON.stringify(jsonObject);
+
+    }
+
+    vaultFromJson(json:string): Map<string, Map<string, string>>{
+
+        // Convertir la chaîne JSON en objet JSON
+        const jsonObject = JSON.parse(json);
+
+        // Convertir l'objet JSON en Map TypeScript
+        const map = new Map<string, Map<string, string>>();
+        for (const key in jsonObject) {
+          if (jsonObject.hasOwnProperty(key)) {
+            const nestedObject = jsonObject[key];
+            const nestedMap = new Map<string, string>();
+            for (const nestedKey in nestedObject) {
+              if (nestedObject.hasOwnProperty(nestedKey)) {
+                nestedMap.set(nestedKey, nestedObject[nestedKey]);
+              }
+            }
+            map.set(key, nestedMap);
+          }
+        }
+
+       return map;
+         }
 }
