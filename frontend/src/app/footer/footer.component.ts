a3bdf1e06bb9b31ff0f4f95fe1ae1df2ccf9078c
seaweedbrain <nathanstchepinsky@gmail.com>
2023-07-20 18:25:05 +0200

    Add footer

---
+++ frontend/src/app/footer/footer.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faGlobe, faEnvelope } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.css']
+})
+export class FooterComponent {
+  
+  faGithub = faGithub;
+  faLinkedin = faLinkedin;
+  faGlobe = faGlobe;
+  faEnvelope = faEnvelope;
+  
+      constructor() { }
+
+}
