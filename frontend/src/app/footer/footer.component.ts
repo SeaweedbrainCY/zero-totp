46d47e39eed97e3fc1c7b84db847bb86ec8ebf2c
seaweedbrain <nathanstchepinsky@gmail.com>
2023-07-20 18:56:44 +0200

    Add image and branch name in the footer

--- frontend/src/app/footer/footer.component.ts
+++ frontend/src/app/footer/footer.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import { faGlobe, faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-footer',
@@ -13,6 +14,7 @@ export class FooterComponent {
   faLinkedin = faLinkedin;
   faGlobe = faGlobe;
   faEnvelope = faEnvelope;
+  imageHash = environment.imageHash;
   
       constructor() { }
 
