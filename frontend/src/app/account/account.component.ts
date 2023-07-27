1d9890376dad9874bde62573ba0ad48dacbb678d
Thomas PEREL <thomas.perel@telecom-sudparis.eu>
2023-07-27 15:49:00 +0200

    Front V1 desktop/mobile for account page

--- frontend/src/app/account/account.component.ts
+++ frontend/src/app/account/account.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { faEnvelope, faLock,  faCheck, faUser} from '@fortawesome/free-solid-svg-icons';
+import { UserService } from '../common/User/user.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-account',
   templateUrl: './account.component.html',
   styleUrls: ['./account.component.css']
 })
-export class AccountComponent {
+export class AccountComponent implements OnInit {
+  faUser=faUser;
+  faEnvelope=faEnvelope;
+  faLock=faLock;
+  faCheck=faCheck;
+  isDestroying = false;
+  isModalActive = false;
+  constructor(
+    private userService: UserService,
+    private router: Router,
+    private route: ActivatedRoute,
+    ){}
+
+  
+  ngOnInit(): void {
+    if(this.userService.getId() == null){
+      this.router.navigate(["/login/sessionKilled"], {relativeTo:this.route.root});
+    } else {
+    }
+  }
+
+  deleteAccount(){
+    this.isDestroying = true;
+  }
+
+  modal(){
+    if(!this.isDestroying){
+      this.isModalActive = !this.isModalActive;
+    }
+  }
 
 }
