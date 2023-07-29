9cf2a058d0eb7a29446aabea50fc6f3c77ca3d1e
seaweedbrain <nathanstchepinsky@gmail.com>
2023-07-29 18:37:38 +0200

    Add tests for hash class. 100% coverage

--- api/tests/unit/test_hash_func.py
+++ api/tests/unit/test_hash_func.py
@@ -12,13 +12,22 @@ class TestBcrypt(unittest.TestCase):
         self.bcrypt.password = "a" * 73
         self.assertRaises(ValueError, self.bcrypt.hashpw)
 
-    def test_hashpw(self):
+    def test_hashpw_same_pw(self):
         self.assertEqual(self.bcrypt.hashpw(), bcrypt.hashpw("password".encode("utf-8"), self.salt))
+    
+    def test_hashpw_diff_pw(self):
         self.assertNotEqual(self.bcrypt.hashpw(), bcrypt.hashpw("wrongpassword".encode("utf-8"), self.salt))
 
-    def test_checkpw(self):
+    def test_checkpw_correct(self):
         self.assertTrue(self.bcrypt.checkpw(bcrypt.hashpw("password".encode("utf-8"), self.salt).decode("utf-8")))
+    
+    def test_checkpw_incorrect(self):
         self.assertFalse(self.bcrypt.checkpw(bcrypt.hashpw("wrongpassword".encode("utf-8"), self.salt).decode("utf-8")))
+    
+    def test_checkpw_incorrect_type(self):
+        falsePassword =  "a" * 73
+        self.assertFalse(self.bcrypt.checkpw( falsePassword))
+
 
 if __name__ == '__main__':
     unittest.main()
