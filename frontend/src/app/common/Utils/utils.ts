3a18eb0199af43bf8875e9eaa2aef5cfd0e0b331
seaweedbrain <nathanstchepinsky@gmail.com>
2023-07-29 17:29:36 +0200

    Authorize & char

--- frontend/src/app/common/Utils/utils.ts
+++ frontend/src/app/common/Utils/utils.ts
@@ -7,7 +7,7 @@ export class Utils {
         if(unsafe_str == null){
             return null;
         }
-        return unsafe_str.replaceAll('&', '&amp;').replaceAll('<', '&lt;').replaceAll('>', '&gt;').replaceAll('"', '&quot;').replaceAll("'", '&#039;').replaceAll('/', '&#x2F;').replaceAll('`', '&#x60;').replaceAll('=', '&#x3D;');
+        return unsafe_str.replaceAll('"', '&quot;').replaceAll("'", '&#039;').replaceAll('/', '&#x2F;').replaceAll('`', '&#x60;').replaceAll('=', '&#x3D;');
     }
 
     passwordSanitize(unsafe_str:string|null) : string |null {
