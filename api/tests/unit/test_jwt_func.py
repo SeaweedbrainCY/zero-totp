8e0cf385002b5ae9e27fe381628e3a5931285eab
seaweedbrain <nathanstchepinsky@gmail.com>
2023-07-30 17:58:41 +0200

    Add units tests for jwt_func

---
+++ api/tests/unit/test_jwt_func.py
@@ -0,0 +1,69 @@
+import jwt 
+import unittest
+from Crypto.jwt_func import verify_jwt, generate_jwt
+import datetime
+import environment as env
+from werkzeug.exceptions import Forbidden
+
+
+class TestJWT(unittest.TestCase):
+
+    def setUp(self):
+        self.timezone = datetime.timezone.utc
+        self.timeFormat = "%Y-%m-%d %H:%M:%S"
+        self.validPayload = {
+            "iss": "https://api.zero-totp.com",
+            "sub": 1,
+            "iat": datetime.datetime.utcnow(),
+            "nbf": datetime.datetime.utcnow(),
+            "exp": datetime.datetime.utcnow() + datetime.timedelta(hours=1),
+        }
+        self.secret = env.jwt_secret
+        self.algorithm = "HS256"
+
+    def test_verify_jwt_correct(self):
+        validJWT = jwt.encode(self.validPayload, self.secret, algorithm=self.algorithm)
+        data_verified = verify_jwt(validJWT)
+        self.assertEqual(data_verified["sub"], 1)
+    
+
+    def test_verify_jwt_invalid_iss(self):
+        self.validPayload["iss"] = "https://evil.com"
+        validJWT = jwt.encode(self.validPayload, self.secret, algorithm=self.algorithm)
+        self.assertRaises(Forbidden, verify_jwt, validJWT)
+    
+    def test_verify_jwt_invalid_iat(self):
+        self.validPayload["iat"] = datetime.datetime.utcnow() + datetime.timedelta(hours=1)
+        validJWT = jwt.encode(self.validPayload, self.secret, algorithm=self.algorithm)
+        self.assertRaises(Forbidden, verify_jwt, validJWT)
+
+    def test_verify_jwt_invalid_nbf(self):
+        self.validPayload["nbf"] = datetime.datetime.utcnow() + datetime.timedelta(hours=1)
+        validJWT = jwt.encode(self.validPayload, self.secret, algorithm=self.algorithm)
+        self.assertRaises(Forbidden, verify_jwt, validJWT)
+    
+
+    def test_verify_jwt_invalid_exp(self):
+        self.validPayload["exp"] = datetime.datetime.utcnow() - datetime.timedelta(hours=1)
+        validJWT = jwt.encode(self.validPayload, self.secret, algorithm=self.algorithm)
+        self.assertRaises(Forbidden, verify_jwt, validJWT)
+    
+
+    def test_verify_jwt_invalid_signature(self):
+        self.secret = "anothersecret"
+        validJWT = jwt.encode(self.validPayload, self.secret, algorithm=self.algorithm)
+        self.assertRaises(Forbidden, verify_jwt, validJWT)
+
+    
+
+    def test_generate_jwt(self):
+        jwt = generate_jwt(1)
+        self.assertTrue(jwt)
+        self.assertTrue(verify_jwt(jwt))
+        self.assertEqual(verify_jwt(jwt)["sub"], 1)
+
+    def test_generate_jwt_invalid_key(self):
+        realSecret = env.jwt_secret
+        env.jwt_secret = "-----BEGIN PUBLIC KEY-----\nMFswDQYJKoZIhvcNAQEBBQADSgAwRwJAQqbu/gXebwVHrK9DAh/yeMu7Hw7P0HC4sgwE88Kep51c/WDeAJsd9NHd5AM3Omq1f8A2SP6kPP5sC7kI7douswIDAQAB\n-----END PUBLIC KEY-----"
+        self.assertRaises(Exception, generate_jwt, 1)
+        env.jwt_secret = realSecret
