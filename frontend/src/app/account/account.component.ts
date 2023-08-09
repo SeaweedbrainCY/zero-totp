810d0e25d66286afa9a8c8e1342bc50eccaa574e
seaweedbrain <nathanstchepinsky@gmail.com>
2023-08-09 12:37:12 +0200

    Add passphrase hashing and beginning of passphrase update

--- frontend/src/app/account/account.component.ts
+++ frontend/src/app/account/account.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { toast as superToast } from 'bulma-toast'
-import { faEnvelope, faLock,  faCheck, faUser} from '@fortawesome/free-solid-svg-icons';
+import { faEnvelope, faLock,  faCheck, faUser, faCog} from '@fortawesome/free-solid-svg-icons';
 import { UserService } from '../common/User/user.service';
 import { HttpClient } from '@angular/common/http';
 import { ApiService } from '../common/ApiService/api-service';
 import { Utils } from '../common/Utils/utils';
 import { ActivatedRoute, Router } from '@angular/router';
 import { error } from 'console';
+import { Crypto } from '../common/Crypto/crypto';
+import { resolve } from 'path';
+import { rejects } from 'assert';
 
 @Component({
   selector: 'app-account',
@@ -18,6 +21,7 @@ export class AccountComponent implements OnInit {
   faEnvelope=faEnvelope;
   faLock=faLock;
   faCheck=faCheck;
+  faCog=faCog;
   isDeletionModalActive=false;
   isPassphraseModalActive=false;
   buttonLoading = {"email":0, "username":0, "passphrase":0, "deletion":0}
@@ -31,12 +35,15 @@ export class AccountComponent implements OnInit {
   confirmNewPassword="";
   newPasswordErrorMessage : [string]=[""];
   newPasswordConfirmErrorMessage : [string]=[""];
+  step =0;
+  password="";
   constructor(
     private http: HttpClient,
     public userService: UserService,
     private utils: Utils,
     private router: Router,
     private route: ActivatedRoute,
+    private crypto:Crypto
     ){}
 
   
@@ -125,24 +132,47 @@ export class AccountComponent implements OnInit {
     const special = /[`!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?~]/;
     const upper = /[A-Z]/;
     const number = /[0-9]/;
+    const forbidden = /["\'<>]/
+    let isOk = true;
+    if(this.password == ""){
+      superToast({
+        message: "We need your former passphrase to update your passphrase",
+        type: "is-danger",
+        dismissible: false,
+        duration: 20000,
+        animate: { in: 'fadeIn', out: 'fadeOut' }
+      });
+      isOk = false;
+    }
+    if(forbidden.test(this.newPassword)){
+      this.newPasswordErrorMessage.push("' \" < > characters are forbidden in passwords");
+      isOk = false;
+    }
     if(this.newPassword.length < 8){
       this.newPasswordErrorMessage.push("Your password must be at least 8 characters long");
+      isOk = false;
     }
     else if(this.newPassword.length > 70){
       this.newPasswordErrorMessage.push("Password must be less than 70 characters long");
+      isOk = false;
     }
     if(!special.test(this.newPassword)){
       this.newPasswordErrorMessage.push("Your password must contain at least one special character");
+      isOk = false;
     }
     if(!upper.test(this.newPassword)){
       this.newPasswordErrorMessage.push("Your password must contain at least one uppercase character");
+      isOk = false;
     }
     if(!number.test(this.newPassword)){
       this.newPasswordErrorMessage.push("Your password must contain at least one number");
+      isOk = false;
     }
     if(this.newPassword != "" && this.confirmNewPassword != "" && this.newPassword != this.confirmNewPassword){
       this.newPasswordConfirmErrorMessage.push("Your passwords do not match");
+      isOk = false;
     }
+    return isOk;
   }
 
 
@@ -151,7 +181,51 @@ export class AccountComponent implements OnInit {
   }
 
   updatePassphrase(){
+    this.step=0;
+    this.buttonLoading["passphrase"] = 0
+    if(!this.checkNewPassword()){
+      return;
+    }
+
+    this.passphraseModal();
+  }
+
+  updatePassphraseConfirm(){
     this.buttonLoading["passphrase"] = 1
+    this.hashPassword().then(hashed => {
+      
+    }, error => {
+      this.buttonLoading["passphrase"] = 0;
+    });
+  }
+
+
+
+  hashPassword(): Promise<string>{
+    return new Promise<string>((resolve, reject) => {
+    const salt = this.userService.getPassphraseSalt();
+    if (salt == null){
+      this.router.navigate(["/login/sessionKilled"], {relativeTo:this.route.root});
+    } else {
+      this.crypto.hashPassphrase(this.password, salt).then  (hashed => {
+        if(hashed != null){
+            resolve(hashed);
+        } else {
+          superToast({
+            message: "An error occured while hashing your password. Please, try again",
+            type: "is-danger",
+            dismissible: false,
+            duration: 20000,
+            animate: { in: 'fadeIn', out: 'fadeOut' }
+          });
+            reject("hashed is null");
+        }
+      });
+      }
+    });
+  }
+
+  verifyPassword():Promise<string>{
   }
 
   deletionModal(){
