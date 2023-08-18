e503115c6f443259db5da674eaa35db46558ed0c
seaweedbrain <nathanstchepinsky@gmail.com>
2023-08-18 10:34:18 +0200

    Add icon for storage options

--- frontend/src/app/vault/vault.component.ts
+++ frontend/src/app/vault/vault.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../common/User/user.service';
 import { ActivatedRoute, Router } from '@angular/router';
-import { faPen, faSquarePlus, faCopy, faCheckCircle, faCircleXmark, faDownload, faDesktop, faRotateRight, faChevronUp, faChevronDown } from '@fortawesome/free-solid-svg-icons';
+import { faPen, faSquarePlus, faCopy, faCheckCircle, faCircleXmark, faDownload, faDesktop, faRotateRight, faChevronUp, faChevronDown, faChevronRight, faLink, faCircleInfo, faUpload } from '@fortawesome/free-solid-svg-icons';
 import { faGoogleDrive } from '@fortawesome/free-brands-svg-icons';
 import { HttpClient } from '@angular/common/http';
 import { ApiService } from '../common/ApiService/api-service';
@@ -27,6 +27,10 @@ export class VaultComponent implements OnInit {
   faDownload=faDownload;
   faChevronUp=faChevronUp;
   faChevronDown=faChevronDown;
+  faChevronRight=faChevronRight;
+  faLink=faLink;
+  faCircleInfo=faCircleInfo;
+  faUpload=faUpload;
   vault: Map<string, Map<string,string>> | undefined;
   vaultDomain : string[] = [];
   remainingTime = 0;
