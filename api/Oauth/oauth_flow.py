f6acaca8c43963a54a6a2153bbded0ff9139068f
seaweedbrain <nathanstchepinsky@gmail.com>
2023-08-20 01:32:49 +0200

    Add Oauth flow file, and primitive func

---
+++ api/Oauth/oauth_flow.py
@@ -0,0 +1,57 @@
+import google.oauth2.credentials
+import google_auth_oauthlib.flow
+import environment as env
+
+
+SCOPES = ['https://www.googleapis.com/auth/drive.files']
+
+
+def get_authorization_url():
+    # Use the client_secret.json file to identify the application requesting
+    # authorization. The client ID (from that file) and access scopes are required.
+    flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
+        env.oauth_client_secret_file,
+        scopes=SCOPES)
+
+    # Indicate where the API server will redirect the user after the user completes
+    # the authorization flow. The redirect URI is required. The value must exactly
+    # match one of the authorized redirect URIs for the OAuth 2.0 client, which you
+    # configured in the API Console. If this value doesn't match an authorized URI,
+    # you will get a 'redirect_uri_mismatch' error.
+    flow.redirect_uri = env.callback_URI
+
+    # Generate URL for request to Google's OAuth 2.0 server.
+    # Use kwargs to set optional request parameters.
+    authorization_url, state = flow.authorization_url(
+        # Enable offline access so that you can refresh an access token without
+        # re-prompting the user for permission. Recommended for web server apps.
+        access_type='offline',
+        # Enable incremental authorization. Recommended as a best practice.
+        include_granted_scopes='true')
+    return authorization_url, state
+
+
+def get_credentials(request_url, state):
+    flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
+        env.oauth_client_secret_file,
+        scopes=['https://www.googleapis.com/auth/drive.metadata.readonly'],
+        state=state)
+    flow.redirect_uri = env.callback_URI
+
+    authorization_response = request_url
+    flow.fetch_token(authorization_response=authorization_response) 
+
+    # Store the credentials in the session.
+    # ACTION ITEM for developers:
+    #     Store user's access and refresh tokens in your data store if
+    #     incorporating this code into your real app.
+    credentials = flow.credentials
+    print(credentials)
+    credentials = {
+        'token': credentials.token,
+        'refresh_token': credentials.refresh_token,
+        'token_uri': credentials.token_uri,
+        'client_id': credentials.client_id,
+        'client_secret': credentials.client_secret,
+        'scopes': credentials.scopes}
+    return credentials
