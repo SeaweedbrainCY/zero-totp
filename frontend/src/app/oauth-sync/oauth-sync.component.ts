ba9403892e4ff044a0c1b8ddaca051406b2f0f2c
seaweedbrain <nathanstchepinsky@gmail.com>
2023-08-20 12:13:26 +0200

    Add icon

--- frontend/src/app/oauth-sync/oauth-sync.component.ts
+++ frontend/src/app/oauth-sync/oauth-sync.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { UserService } from '../common/User/user.service';
+import { HttpClient } from '@angular/common/http';
+import { ApiService } from '../common/ApiService/api-service';
+import { faCircleNotch } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-oauth-sync',
   templateUrl: './oauth-sync.component.html',
@@ -8,6 +11,7 @@ import { UserService } from '../common/User/user.service';
 })
 export class OauthSyncComponent implements OnInit {
   errorMessage = '';
+  faCircleNotch = faCircleNotch;
 
   constructor(
     private route: ActivatedRoute,
@@ -19,6 +23,10 @@ export class OauthSyncComponent implements OnInit {
     if(this.userService.getId() == null){
       this.router.navigate(["/login/sessionKilled"], {relativeTo:this.route.root});
   }
+
+  }
+
+  uploadEncryptedTokens(){
 
   }
 }
