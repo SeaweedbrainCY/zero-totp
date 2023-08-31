0b351484326118201ce40f4d548d355afa7a4886
seaweedbrain <nathanstchepinsky@gmail.com>
2023-08-31 15:50:22 -0400

    require both public and private key

--- api/environment.py
+++ api/environment.py
@@ -6,6 +6,7 @@ db_uri = os.environ.get('DATABASE_URI')
 environment = "production" if os.environ.get('ENVIRONMENT') == "production" else "development"
 jwt_secret = os.environ.get('JWT_SECRET')
 private_key_path = os.environ.get('PRIVATE_KEY_PATH')
+public_key_path = os.environ.get('PUBLIC_KEY_PATH')
 
 
 if environment == "development":
@@ -38,6 +39,6 @@ if jwt_secret == None:
     raise Exception("JWT_SECRET environment variable not set. Please set it to a valid secret key.")
 
 
-if private_key_path == None:
-    logging.error("PRIVATE_KEY_PATH environment variable not set. Please set it to a valid private key path. Aborting...")
-    raise Exception("PRIVATE_KEY_PATH environment variable not set. Please set it to a valid private key path.")
+if private_key_path == None or public_key_path == None:
+    logging.error("PRIVATE_KEY_PATH or PUBLIC_KEY_PATH environment variable not set. Please set it to a valid key path. Aborting...")
+    raise Exception("PRIVATE_KEY_PATH or PUBLIC_KEY_PATH environment variable not set. Please set it to a valid key path.")
