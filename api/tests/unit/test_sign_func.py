3d8e17a294b3629729f04ff5a4460cb1704b0aec
seaweedbrain <nathanstchepinsky@gmail.com>
2023-08-31 15:50:46 -0400

    Add tests for signature verification

--- api/tests/unit/test_sign_func.py
+++ api/tests/unit/test_sign_func.py
@@ -1,5 +1,5 @@
 import unittest
-from  CryptoClasses.sign_func import sign
+from  CryptoClasses.sign_func import sign, verify
 
 
 class TestBcrypt(unittest.TestCase):
@@ -10,5 +10,22 @@ class TestBcrypt(unittest.TestCase):
     def test_signature(self):
         message = "Hello World"
         signature = sign(message)
-        print(message, signature)
         self.assertTrue(signature)
+    
+    def test_verifying(self):
+        message = "Hello World"
+        signature = sign(message)
+        self.assertTrue(signature)
+        self.assertTrue(verify(message=message, signature=signature))
+    
+    def test_verifying_wrong_message(self):
+        message = "Hello World"
+        signature = sign(message)
+        self.assertTrue(signature)
+        self.assertFalse(verify(message="Hello World!", signature=signature))
+    
+    def test_verifying_wrong_signature(self):
+        message = "Hello World"
+        signature = sign(message)
+        self.assertTrue(signature)
+        self.assertFalse(verify(message=message, signature="3273a3e8fd23c695671ed7c7d10baa5ae2651c3f0ea5f44"))
