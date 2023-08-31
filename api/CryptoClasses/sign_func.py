cadcd54b913f006cc1025131f09714b3a922b655
seaweedbrain <nathanstchepinsky@gmail.com>
2023-08-31 15:48:55 -0400

    Add signature verification class

--- api/CryptoClasses/sign_func.py
+++ api/CryptoClasses/sign_func.py
@@ -2,6 +2,7 @@ from Crypto.Signature import pkcs1_15
 from Crypto.PublicKey import RSA
 from Crypto.Hash import SHA256
 import environment as env
+import logging
 
 
 def sign(message):
@@ -10,4 +11,16 @@ def sign(message):
         h = SHA256.new(message.encode())
         signature = pkcs1_15.new(private_key).sign(h)
         return signature.hex()
-    
+
+
+def verify(signature, message):
+    with open(env.public_key_path, "r") as key_file:
+        public_key = RSA.import_key(key_file.read())
+        h = SHA256.new(message.encode("utf-8"))
+        try:
+            signature = bytes.fromhex(signature)
+            pkcs1_15.new(public_key).verify(h, signature)
+            return True  
+        except (ValueError, TypeError) as e:
+            logging.warning("Invalid signature " + str(e))
+            return False  
