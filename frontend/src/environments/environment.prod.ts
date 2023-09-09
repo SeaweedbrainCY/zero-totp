b520b3add01798bbd834e4e7b647093c718cbfbc
seaweedbrain <nathanstchepinsky@gmail.com>
2023-09-09 00:16:31 -0400

    Add RSA public key for vault signature verification

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,13 @@
 export const environment = {
     production: true,
-    API_URL : "https://api.zero-totp.com"
+    API_URL : "https://api.zero-totp.com",
+    API_public_key:`-----BEGIN PUBLIC KEY-----
+    MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA1wJTqnZ1efr1g1mMw813
+    Aq97QilCyxrYE9ckYx96gHqh/rd/2k+xwQtPT+pGybyuYQZw7TFLeiLp7wPGmOgu
+    QTD1USKG+kmoG+5QnldYpRDBVIBzgKPs9eGP6Xf5jIANpSNujc3fOU+dsJbSz+Vz
+    mCwu5T1eLsaHRg4rqNJYacPr2OJzo+VKBMAZCQcUQk7vOiNv7cTFzjctKkKj/B27
+    QUHcklVvBWqxboE1g51J2l1jBwaxarciN5M1ZadM2B+uVmi2+Iz7uFomL3yH9djo
+    5ZnWVkkO2hJ+4k9/thvMyAfgZIPtQdBVdM/P85YxT/7GAWzCHQd9rIX40Rs3PzaJ
+    CQIDAQAB
+    -----END PUBLIC KEY-----`
   };
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,14 @@
 export const environment = {
     production: false,
     API_URL : "http://localhost:8080",
-    imageHash : "development mode"
+    imageHash : "development mode",
+    API_public_key:`-----BEGIN PUBLIC KEY-----
+    MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA1wJTqnZ1efr1g1mMw813
+    Aq97QilCyxrYE9ckYx96gHqh/rd/2k+xwQtPT+pGybyuYQZw7TFLeiLp7wPGmOgu
+    QTD1USKG+kmoG+5QnldYpRDBVIBzgKPs9eGP6Xf5jIANpSNujc3fOU+dsJbSz+Vz
+    mCwu5T1eLsaHRg4rqNJYacPr2OJzo+VKBMAZCQcUQk7vOiNv7cTFzjctKkKj/B27
+    QUHcklVvBWqxboE1g51J2l1jBwaxarciN5M1ZadM2B+uVmi2+Iz7uFomL3yH9djo
+    5ZnWVkkO2hJ+4k9/thvMyAfgZIPtQdBVdM/P85YxT/7GAWzCHQd9rIX40Rs3PzaJ
+    CQIDAQAB
+    -----END PUBLIC KEY-----`
   };
