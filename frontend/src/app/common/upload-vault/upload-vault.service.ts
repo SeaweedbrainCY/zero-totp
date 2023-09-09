201f4d3317ef3a94684adc834f694754eeacde75
seaweedbrain <nathanstchepinsky@gmail.com>
2023-09-09 00:20:06 -0400

    Add all steps of verification and sanitazing of the imported vault

--- frontend/src/app/common/upload-vault/upload-vault.service.ts
+++ frontend/src/app/common/upload-vault/upload-vault.service.ts
@@ -1,10 +1,11 @@
-import { Injectable, SecurityContext, Sanitizer } from '@angular/core';
+import { SecurityContext, Sanitizer,Injectable } from '@angular/core';
+import { Crypto } from '../Crypto/crypto';
 
-@Injectable({
-  providedIn: 'root'
-})
 
-enum UploadVaultErrors {
+
+
+export enum UploadVaultStatus {
+  SUCCESS = 0,
   INVALID_JSON = 1,
   INVALID_VERSION = 2,
   NO_SIGNATURE = 3,
@@ -14,53 +15,64 @@ enum UploadVaultErrors {
   UNKNOWN = 7
 }
 
+@Injectable({
+  providedIn: 'root'
+})
 export class UploadVaultService {
 
   constructor(
-    private sanitizer: Sanitizer
+    private sanitizer: Sanitizer,
+    private crypto: Crypto
   ) { }
 
-  parseUploadedVault(unsecure_context:string): Map<string, string> | UploadVaultErrors {
+  parseUploadedVault(unsecure_context_b64:string): [(Map<string, string> | null), UploadVaultStatus] {
     let vault = new Map<string, string>();
     try{
-      let context = JSON.parse(unsecure_context);
+      if(unsecure_context_b64.split(",").length != 2){
+        return [null, UploadVaultStatus.NO_SIGNATURE];
+      }
+      const unsecure_vault_b64 = unsecure_context_b64.split(",")[0];
+      const signature = unsecure_context_b64.split(",")[1];
+      let unsecure_vault = atob(unsecure_vault_b64);
+      let context = JSON.parse(unsecure_vault);
       if(context == null){
-        return UploadVaultErrors.INVALID_JSON;
+        return [null, UploadVaultStatus.INVALID_JSON];
       }
       if(context.hasOwnProperty("version")){
         if(context.version == 1){
-          if(context.hasOwnProperty("signature")){
-          const required_keys = ["version", "date", "derived_key_salt", "zke_key_enc", "secrets", "signature"]
+          const required_keys = ["version", "date", "derived_key_salt", "zke_key_enc", "secrets"]
           for(let key of required_keys){
             if(context.hasOwnProperty(key)){
               const sanitized = this.sanitizer.sanitize(SecurityContext.HTML, context[key])
               if(sanitized != null){
-                vault.set(key, sanitized);
+                this.crypto.verifySignature(unsecure_vault_b64, signature).then((result) => {
+                  if(result){
+                      vault.set(key, sanitized);
+                      return vault;
+                  } else {
+                    return [null,UploadVaultStatus.INVALID_SIGNATURE];
+                  }
+                });
               }else {
-                return UploadVaultErrors.INVALID_ARGUMENT;
+                return [null,UploadVaultStatus.INVALID_ARGUMENT];
               }
             } else {
-              return UploadVaultErrors.MISSING_ARGUMENT;
+              return [null,UploadVaultStatus.MISSING_ARGUMENT];
             }
           }
-          return vault;
+          return [vault, UploadVaultStatus.SUCCESS];
         } else {
-          return UploadVaultErrors.NO_SIGNATURE;
-        }
-        } else {
-          return UploadVaultErrors.INVALID_VERSION;
+          return [null,UploadVaultStatus.INVALID_VERSION];
         }
       } else {
-        return UploadVaultErrors.INVALID_VERSION;
+        return [null,UploadVaultStatus.INVALID_VERSION];
       }
     } 
     catch(e){
-      return UploadVaultErrors.INVALID_JSON;
+      return [null,UploadVaultStatus.INVALID_JSON];
     }
   }
-  verifySignature(context:string):boolean{
-    
-  }
+  
 }
 
 
