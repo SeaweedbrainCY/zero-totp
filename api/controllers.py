8f6e6af3b6344fba64ef800624ca50de729eaf67
seaweedbrain <nathanstchepinsky@gmail.com>
2023-09-09 00:43:16 -0400

    Encode the vault in base64 for better signature performing

--- api/controllers.py
+++ api/controllers.py
@@ -389,8 +389,9 @@ def export_vault():
     for secret in totp_secrets_list:
         secrets[secret.uuid] = secret.secret_enc
     vault["secrets"] = secrets
-    signature = api_signature.sign(json.dumps(secrets))
-    vault["signature"] = signature
+    vault_b64 = base64.b64encode(json.dumps(vault).encode("utf-8")).decode("utf-8")
+    signature = api_signature.sign(vault_b64)
+    vault = vault_b64 + "," + signature
     return vault, 200
 
     
