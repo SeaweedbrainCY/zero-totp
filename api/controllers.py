cc391512976eef206738abdd9f4efd0d21cfffdf
seaweedbrain <nathanstchepinsky@gmail.com>
2023-09-12 14:21:03 -0400

    Resolve conflicts

--- api/controllers.py
+++ api/controllers.py
@@ -1,4 +1,5 @@
-from flask import request, Response
+from flask import request, Response, redirect, make_response
+import flask
 import connexion
 import json
 from database.user_repo import User as UserDB
@@ -11,6 +12,8 @@ import random
 import string
 import CryptoClasses.jwt_func as jwt_auth
 import CryptoClasses.sign_func as api_signature
+import Crypto.jwt_func as jwt_auth
+import Oauth.oauth_flow as oauth_flow
 import Utils.utils as utils
 import os
 import base64
@@ -395,3 +398,19 @@ def export_vault():
     return vault, 200
 
     
+# GET /oauth/authorization_flow
+def get_authorization_flow():
+    authorization_url, state = oauth_flow.get_authorization_url()
+    flask.session["state"] = state
+    return {"authorization_url": authorization_url}, 200
+
+# GET /oauth/callback
+def oauth_callback():
+    #TODO store token URI
+    #TODO get expiration date
+    #TODO handle errors
+    credentials = oauth_flow.get_credentials(request.url, flask.session["state"])
+    response = make_response(redirect(env.frontend_URI + "/callback", code=302))
+    response.set_cookie("google_drive_token_id", credentials["token"], httponly=False, secure=True, samesite="Strict")
+    response.set_cookie("google_drive_refresh_token", credentials["refresh_token"], httponly=False, secure=True, samesite="Strict")
+    return response
