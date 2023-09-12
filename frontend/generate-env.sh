1b14ca587351a8d1314a8d5bb66f6372c4578574
seaweedbrain <nathanstchepinsky@gmail.com>
2023-09-12 12:48:31 -0400

    Fix script

--- frontend/generate-env.sh
+++ frontend/generate-env.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-echo "export const environment = {production: true,API_URL : \"https://api.zero-totp.com\",imageHash : \"$1\", API_public_key=\"$2\"};" > src/environments/environment.prod.ts
+echo "export const environment = {production: true,API_URL : \"https://api.zero-totp.com\",imageHash : \"$1\", API_public_key:\"$2\"};" > src/environments/environment.prod.ts
