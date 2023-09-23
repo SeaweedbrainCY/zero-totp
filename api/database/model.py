a172ff4dc72c06851496b24f5dc62d291755e077
seaweedbrain <nathanstchepinsky@gmail.com>
2023-09-23 15:17:39 -0400

    Change oauth model, by storing the encrypted creds json instead of tokens

--- api/database/model.py
+++ api/database/model.py
@@ -38,7 +38,5 @@ class Oauth_tokens(db.Model):
     __tablename__ = "oauth_tokens"
     id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey("User.id"), nullable=False)
-    access_token_enc = db.Column(db.Text, nullable=False)
-    refresh_token_enc = db.Column(db.Text, nullable=False)
+    enc_credentials = db.Column(db.Text, nullable=False)
     expires_at = db.Column(db.Integer, nullable=False)
-    token_uri = db.Column(db.String(256), nullable=False)
