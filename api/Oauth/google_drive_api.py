d96257998854cecb54c3db76c33660c8e138b868
seaweedbrain <nathanstchepinsky@gmail.com>
2023-09-24 11:56:33 -0400

    Add successfull connection to google API

--- api/Oauth/google_drive_api.py
+++ api/Oauth/google_drive_api.py
@@ -3,9 +3,16 @@ import google_auth_oauthlib.flow
 import environment as env
 import logging
 from googleapiclient.discovery import build
+from google.oauth2.credentials import Credentials
+from datetime import datetime
 
 
 
-def backup(enc_vault, refresh_token, access_token):
-    drive = build('drive', 'v2', credentials=credentials)
-    
+
+
+def backup(credentials):
+        dat_obj = datetime.strptime(credentials["expiry"], '%Y-%m-%d %H:%M:%S.%f')
+        credentials["expiry"] = dat_obj.strftime('%Y-%m-%dT%H:%M:%SZ')
+        creds = Credentials.from_authorized_user_info(credentials)
+        drive = build('drive', 'v2', credentials=creds)
+        #files = drive.files().list().execute()
