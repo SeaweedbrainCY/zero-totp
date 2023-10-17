30bfaf9a80cc0b8a6355d13523023dd446908d17
seaweedbrain <nathanstchepinsky@gmail.com>
2023-10-17 15:19:21 -0400

    Fix return types in security wrapper

--- api/Utils/security_wrapper.py
+++ api/Utils/security_wrapper.py
@@ -1,19 +1,17 @@
 from database.user_repo import User as UserDB
 import connexion
-from werkzeug.exceptions import Unauthorized, BadRequest
-
 
 def admin_restricted(func):
     def wrapper(*args, **kwargs):
         try:
             user_id = connexion.context.get("user")
         except:
-            raise BadRequest()
+            return {"error": "Unauthorized"}, 401
         user = UserDB().getById(user_id)
         if user == None:
-            return Unauthorized()
+            return {"error": "Forbidden"}, 403
         if user.role == "admin":
             return func(*args, **kwargs)
-        return Unauthorized()
+        return {"error": "Unauthorized"}, 403
     return wrapper
         
