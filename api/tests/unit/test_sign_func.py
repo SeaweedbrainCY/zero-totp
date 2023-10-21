b412e7d1fe7401478fa680d2c61a107713453068
seaweedbrain <nathanstchepinsky@gmail.com>
2023-10-20 20:28:24 -0400

    Adapt tests to the new API signature functions

--- api/tests/unit/test_sign_func.py
+++ api/tests/unit/test_sign_func.py
@@ -1,5 +1,5 @@
 import unittest
-from  CryptoClasses.sign_func import sign, verify
+from  CryptoClasses.sign_func import API_signature, AdminSignature
 
 
 class TestBcrypt(unittest.TestCase):
@@ -9,23 +9,23 @@ class TestBcrypt(unittest.TestCase):
 
     def test_signature(self):
         message = "Hello World"
-        signature = sign(message)
+        signature = API_signature.sign_rsa(message)
         self.assertTrue(signature)
     
     def test_verifying(self):
         message = "Hello World"
-        signature = sign(message)
+        signature = API_signature.sign_rsa(message)
         self.assertTrue(signature)
-        self.assertTrue(verify(message=message, signature=signature))
+        self.assertTrue(API_signature.verify_rsa_signature(message=message, signature=signature))
     
     def test_verifying_wrong_message(self):
         message = "Hello World"
-        signature = sign(message)
+        signature = API_signature.sign_rsa(message)
         self.assertTrue(signature)
-        self.assertFalse(verify(message="Hello World!", signature=signature))
+        self.assertFalse(API_signature.verify_rsa_signature(message="Hello World!", signature=signature))
     
     def test_verifying_wrong_signature(self):
         message = "Hello World"
-        signature = sign(message)
+        signature = API_signature.sign_rsa(message)
         self.assertTrue(signature)
-        self.assertFalse(verify(message=message, signature="3273a3e8fd23c695671ed7c7d10baa5ae2651c3f0ea5f44"))
+        self.assertFalse(API_signature.verify_rsa_signature(message=message, signature="3273a3e8fd23c695671ed7c7d10baa5ae2651c3f0ea5f44"))
