c9f7a99c876d41ac19e1a6dad97008aa599162d6
seaweedbrain <nathanstchepinsky@gmail.com>
2023-10-21 18:56:06 -0400

    Fix tests

--- api/tests/unit/test_sign_func.py
+++ api/tests/unit/test_sign_func.py
@@ -5,27 +5,27 @@ from  CryptoClasses.sign_func import API_signature, AdminSignature
 class TestBcrypt(unittest.TestCase):
     
     def setUp(self):
-        pass
+        self.api_signature = API_signature()
 
     def test_signature(self):
         message = "Hello World"
-        signature = API_signature.sign_rsa(message)
+        signature = self.api_signature.sign_rsa(message)
         self.assertTrue(signature)
     
     def test_verifying(self):
         message = "Hello World"
-        signature = API_signature.sign_rsa(message)
+        signature = self.api_signature.sign_rsa(message)
         self.assertTrue(signature)
-        self.assertTrue(API_signature.verify_rsa_signature(message=message, signature=signature))
+        self.assertTrue(self.api_signature.verify_rsa_signature(message=message, signature=signature))
     
     def test_verifying_wrong_message(self):
         message = "Hello World"
-        signature = API_signature.sign_rsa(message)
+        signature = self.api_signature.sign_rsa(message)
         self.assertTrue(signature)
-        self.assertFalse(API_signature.verify_rsa_signature(message="Hello World!", signature=signature))
+        self.assertFalse(self.api_signature.verify_rsa_signature(message="Hello World!", signature=signature))
     
     def test_verifying_wrong_signature(self):
         message = "Hello World"
-        signature = API_signature.sign_rsa(message)
+        signature = self.api_signature.sign_rsa(message)
         self.assertTrue(signature)
-        self.assertFalse(API_signature.verify_rsa_signature(message=message, signature="3273a3e8fd23c695671ed7c7d10baa5ae2651c3f0ea5f44"))
+        self.assertFalse(self.api_signature.verify_rsa_signature(message=message, signature="3273a3e8fd23c695671ed7c7d10baa5ae2651c3f0ea5f44"))
