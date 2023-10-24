c2e148f581e58fe4f78d740e1cde26b776580151
seaweedbrain <nathanstchepinsky@gmail.com>
2023-10-24 10:55:06 -0400

    Improve email regex and input sanitizing

--- api/Utils/utils.py
+++ api/Utils/utils.py
@@ -2,18 +2,11 @@ import re
 import html
 
 def check_email(email):
-    forbidden = '["\'<>]'
-    return re.match(r'\S+@\S+\.\S+', email) and not re.search(forbidden, email)
+    email_regex = r"(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])"
+    return re.match(email_regex, email) and re.search(email_regex, email).group() == email
 
-def check_password(password):
-   forbidden = '["\'<>]'
-   return  not re.search(forbidden, password) 
 
-def check_username(username):
-    forbidden = '["\'<>]'
-    return not re.search(forbidden, username)
-
-def escape(string):
+def sanitize_input(string) -> str:
     string = string.replace("'", "")
     string = string.replace('"', "")
     return html.escape(string)
