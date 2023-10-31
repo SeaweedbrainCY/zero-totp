519a88dfa46b15077d9dcbd4c2733da019e7e182
seaweedbrain <nathanstchepinsky@gmail.com>
2023-10-31 15:04:46 -0400

    Add a better logging for the API

--- api/start.sh
+++ api/start.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-gunicorn --bind 0.0.0.0:8080 app:app
+gunicorn --bind 0.0.0.0:8080 app:app -log-level debug --error-logfile /var/log/gunicorn_error.log --access-logfile /var/log/gunicorn_access.log --capture-output --enable-stdio-inheritance
