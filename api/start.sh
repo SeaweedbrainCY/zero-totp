e18f67de00f97df416fca59d0d8fe8e78117318e
seaweedbrain <nathanstchepinsky@gmail.com>
2023-11-01 10:03:28 -0400

    Send all gunicorn log to file

--- api/start.sh
+++ api/start.sh
@@ -1,2 +1,5 @@
 #!/usr/bin/env bash
-gunicorn --bind 0.0.0.0:8080 app:app --log-level debug --error-logfile /var/log/gunicorn_error.log --access-logfile /var/log/gunicorn_access.log --capture-output --enable-stdio-inheritance
+echo "⚡️  Starting gunicorn"
+gunicorn --bind 0.0.0.0:8080 app:app --log-level debug --error-logfile /var/log/api/gunicorn_error.log --access-logfile /var/log/api/gunicorn_access.log --capture-output --enable-stdio-inheritance
+echo "🚀  API started"
+echo "🍺  All logs are in /var/log/api"
