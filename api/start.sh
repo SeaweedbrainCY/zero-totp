e00b3f49bb0fac39bc952061dc147acf2675203b
seaweedbrain <nathanstchepinsky@gmail.com>
2023-11-01 10:27:28 -0400

    Delete the debug level log for gunicorn

--- api/start.sh
+++ api/start.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 echo "⚡️  Starting gunicorn"
-gunicorn --bind 0.0.0.0:8080 app:app --log-level debug --error-logfile /var/log/api/gunicorn_error.log --access-logfile /var/log/api/gunicorn_access.log --capture-output --enable-stdio-inheritance
+gunicorn --bind 0.0.0.0:8080 app:app --error-logfile /var/log/api/gunicorn_error.log --access-logfile /var/log/api/gunicorn_access.log --capture-output --enable-stdio-inheritance
 echo "🚀  API started"
 echo "🍺  All logs are in /var/log/api"
