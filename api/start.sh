6f21a8b9dc3a6e557b85961010c066cd5008a07c
seaweedbrain <nathanstchepinsky@gmail.com>
2023-11-01 10:41:36 -0400

    Change the messages order at start

--- api/start.sh
+++ api/start.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-echo "⚡️  Starting gunicorn"
-gunicorn --bind 0.0.0.0:8080 app:app --error-logfile /var/log/api/gunicorn_error.log --access-logfile /var/log/api/gunicorn_access.log --capture-output --enable-stdio-inheritance
-echo "🚀  API started"
 echo "🍺  All logs are in /var/log/api"
+echo "🚀  Starting gunicorn"
+gunicorn --bind 0.0.0.0:8080 app:app --error-logfile /var/log/api/gunicorn_error.log --access-logfile /var/log/api/gunicorn_access.log --capture-output --enable-stdio-inheritance
+echo "❌  If you see this, gunicorn has crashed. Check the logs (/var/log/api/gunicorn*.log)"
