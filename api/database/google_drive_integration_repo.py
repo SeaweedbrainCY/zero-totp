3c84c2f88cc7be3695db3b51aa5cb7cdade0d5f4
seaweedbrain <nathanstchepinsky@gmail.com>
2023-11-05 17:48:10 -0500

    Add repo for the google drive integration table

---
+++ api/database/google_drive_integration_repo.py
@@ -0,0 +1,41 @@
+from database.db import db 
+from database.model import GoogleDriveIntegration as GoogleDriveIntegrationModel
+from environment import logging
+
+class GoogleDriveIntegration:
+    
+
+    def is_google_drive_enabled(self, user_id):
+        integration = db.session.query(GoogleDriveIntegrationModel).filter_by(user_id=user_id).first()
+        if integration == None:
+             return False
+        return integration.isEnabled
+    
+    def update_google_drive_sync(self, user_id, google_drive_sync):
+        integration = db.session.query(GoogleDriveIntegrationModel).filter_by(user_id=user_id).first()
+        if integration == None:
+            self.create(user_id, google_drive_sync)
+        integration.isEnabled = google_drive_sync
+        db.session.commit()
+        return integration
+    
+    def update_last_backup_clean_date(self, user_id, last_backup_clean_date):
+        integration = db.session.query(GoogleDriveIntegrationModel).filter_by(user_id=user_id).first()
+        if integration == None:
+            return None
+        integration.lastBackupCleanDate = last_backup_clean_date
+        db.session.commit()
+        return integration
+    
+    def get_last_backup_clean_date(self, user_id):
+        integration = db.session.query(GoogleDriveIntegrationModel).filter_by(user_id=user_id).first()
+        if integration == None:
+            return None
+        return integration.lastBackupCleanDate
+    
+    def create(self, user_id, google_drive_sync):
+        integration = GoogleDriveIntegrationModel(user_id=user_id)
+        integration.isEnabled = google_drive_sync
+        db.session.add(integration)
+        db.session.commit()
+        return integration
