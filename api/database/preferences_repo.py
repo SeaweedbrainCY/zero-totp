cc1e4fabc82a0940e224b0e87b3b5c262cee10e7
seaweedbrain <nathanstchepinsky@gmail.com>
2023-11-15 23:16:34 -0500

    Add repo to update each preference

--- api/database/preferences_repo.py
+++ api/database/preferences_repo.py
@@ -16,5 +16,38 @@ class Preferences:
         if pref == None:
             pref = self.create_default_preferences(user_id)
         return pref
+    
+    def update_favicon(self, user_id, favicon_policy):
+        pref = db.session.query(PreferencesModel).filter_by(user_id=user_id).first()
+        if pref == None:
+            pref = self.create_default_preferences(user_id)
+        pref.favicon_preview_policy = favicon_policy
+        db.session.commit()
+        return pref
+    
+    def update_derivation_iteration(self, user_id, derivation_iteration):
+        pref = db.session.query(PreferencesModel).filter_by(user_id=user_id).first()
+        if pref == None:
+            pref = self.create_default_preferences(user_id)
+        pref.derivation_iteration = derivation_iteration
+        db.session.commit()
+        return pref
+    
+    def update_minimum_backup_kept(self, user_id, minimum_backup_kept):
+        pref = db.session.query(PreferencesModel).filter_by(user_id=user_id).first()
+        if pref == None:
+            pref = self.create_default_preferences(user_id)
+        pref.minimum_backup_kept = minimum_backup_kept
+        db.session.commit()
+        return pref
+    
+    def update_backup_lifetile(self, user_id, backup_lifetime):
+        pref = db.session.query(PreferencesModel).filter_by(user_id=user_id).first()
+        if pref == None:
+            pref = self.create_default_preferences(user_id)
+        pref.backup_lifetime = backup_lifetime
+        db.session.commit()
+        return pref
+    
 
     
