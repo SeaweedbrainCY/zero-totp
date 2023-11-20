f123e200236cea868ab85dc40244e1e991bb0e2f
seaweedbrain <nathanstchepinsky@gmail.com>
2023-11-19 22:12:37 -0500

    Use gunicon with ASGI server

--- api/start.sh
+++ api/start.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 echo "🍺  All logs are in /var/log/api"
 echo "🚀  Starting gunicorn"
-gunicorn --bind 0.0.0.0:8080 app:app --error-logfile /var/log/api/gunicorn_error.log --access-logfile /var/log/api/gunicorn_access.log --capture-output --enable-stdio-inheritance
+gunicorn --bind 0.0.0.0:8080 app:app --error-logfile /var/log/api/gunicorn_error.log --access-logfile /var/log/api/gunicorn_access.log --capture-output --enable-stdio-inheritance -k uvicorn.workers.UvicornWorker
 echo "❌  If you see this, gunicorn has crashed. Check the logs (/var/log/api/gunicorn*.log)"
