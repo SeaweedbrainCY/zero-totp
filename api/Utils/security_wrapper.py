46d1a63ba707d3bfb7f63b45e35660454656e73b
seaweedbrain <nathanstchepinsky@gmail.com>
2023-11-29 16:04:41 -0500

    Add a wrapper to verify the user hashed passphrase send in header For critical actions such as delete account

--- api/Utils/security_wrapper.py
+++ api/Utils/security_wrapper.py
@@ -2,6 +2,9 @@ from database.user_repo import User as UserDB
 from CryptoClasses.jwt_func import verify_jwt
 import connexion
 from environment import logging
+from CryptoClasses.hash_func import Bcrypt
+import random
+import string
 
 def require_admin_role(func):
     def wrapper(context_, user, token_info,*args, **kwargs):
@@ -60,3 +63,30 @@ def require_userid(func):
             return {"error": "Unauthorized"}, 401
         return func(user_id, *args, **kwargs)
     return wrapper
+
+
+# By design, the user id is required and checked before the user token.
+# Require x-hash-passphrase header and check it against the user's hashed passphrase
+def require_passphrase_verification(func):
+    def wrapper(context_, user, token_info, *args, **kwargs):
+        try:
+            user_id = context_["user"]
+            if user_id == None:
+                return {"error": "Unauthorized"}, 401
+        except:
+            return {"error": "Unauthorized"}, 401
+        try:
+            passphrase = connexion.request.headers["x-hash-passphrase"]
+        except:
+            return {"error": "Unauthorized"}, 401
+        user_obj = UserDB().getById(user_id)
+        bcrypt = Bcrypt(passphrase)
+
+        if user_obj == None:
+            logging.info("User " + str(user_id) + " tried to login but does not exist. A fake password is checked to avoid timing attacks")
+            fake_password = ''.join(random.choices(string.ascii_letters, k=random.randint(10, 20)))
+            bcrypt.checkpw(fake_password)
+            return {"message": "Invalid credentials"}, 403
+        if bcrypt.checkpw(user.password):
+            return func(user_id, *args, **kwargs)
+        return {"error": "Unauthorized"}, 403
