368650bb4b5cb44e3c69f196898665c34ddad9f5
seaweedbrain <nathanstchepinsky@gmail.com>
2023-12-02 17:32:15 -0500

    Add email verification route

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -17,6 +17,7 @@ import { CallbackComponent } from './callback/callback.component';
 import { OauthSyncComponent } from './oauth-sync/oauth-sync.component';
 import { PreferencesComponent } from './preferences/preferences.component';
 import { OpenSourceLibraryComponent } from './open-source-library/open-source-library.component';
+import { EmailVerificationComponent } from './email-verification/email-verification.component';
 
 const routes: Routes = [
   {path:'', component: HomeComponent},
@@ -37,6 +38,7 @@ const routes: Routes = [
   {path:"oauth/synchronize", component: OauthSyncComponent},
   {path: "preferences", component: PreferencesComponent},
   {path: "opensource", component: OpenSourceLibraryComponent},
+  {path:"emailVerification", component: EmailVerificationComponent},
   {path:'**', component: PagenotfoundComponent}
 ];
 
