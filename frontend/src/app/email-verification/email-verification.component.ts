bfff9fa4a995cc2b8f27598d60d33278f0877208
seaweedbrain <nathanstchepinsky@gmail.com>
2023-12-02 17:31:50 -0500

    Add email verification component

---
+++ frontend/src/app/email-verification/email-verification.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-email-verification',
+  templateUrl: './email-verification.component.html',
+  styleUrls: ['./email-verification.component.css']
+})
+export class EmailVerificationComponent {
+
+}
