76c98f090d862b6f5c2678cecedc01898d702e2c
seaweedbrain <nathanstchepinsky@gmail.com>
2023-12-05 13:36:22 -0500

    Improve style

--- frontend/src/app/email-verification/email-verification.component.ts
+++ frontend/src/app/email-verification/email-verification.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
-
+import { faPaperPlane } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-email-verification',
   templateUrl: './email-verification.component.html',
   styleUrls: ['./email-verification.component.css']
 })
 export class EmailVerificationComponent {
+  faPaperPlane = faPaperPlane;
+  constructor() { }
 
 }
