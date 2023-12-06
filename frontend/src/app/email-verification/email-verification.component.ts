5f1d2c5b0d08a1f67540d260328bdc2012c42333
seaweedbrain <nathanstchepinsky@gmail.com>
2023-12-06 16:59:16 -0500

    Add resend code feature

--- frontend/src/app/email-verification/email-verification.component.ts
+++ frontend/src/app/email-verification/email-verification.component.ts
@@ -19,6 +19,7 @@ export class EmailVerificationComponent {
   emailAddress: string | null = null;
   code = "";
   errorMessage = "";
+  verifyLoading = false;
   constructor(
     private userService: UserService,
     private http: HttpClient,
@@ -26,16 +27,21 @@ export class EmailVerificationComponent {
     private route: ActivatedRoute,
   ) { 
     this.emailAddress = this.userService.getEmail();
+    if (this.emailAddress == null) {
+      this.router.navigate(['/login'], { queryParams: { returnUrl: this.router.url } });
+    }
   }
 
 
   verify(){
     this.errorMessage = '';
+    this.verifyLoading = true;
     const data = {
       "token": this.code
     }
     this.http.put(ApiService.API_URL+"/email/verify",  data, {withCredentials: true, observe: 'response'}).subscribe((response) => {
       if(response.status == 200){
+        this.verifyLoading = false;
         superToast({
           message: "Your email is verified ! ✅",
           type: "is-success",
@@ -43,8 +49,10 @@ export class EmailVerificationComponent {
         animate: { in: 'fadeIn', out: 'fadeOut' }
         });
         this.router.navigate(['/login']);
+
       }
     }, (error) => {
+      this.verifyLoading = false;
       if(error.status == 403){
         this.errorMessage = "Invalid or expired code";
       } else {
@@ -55,6 +63,27 @@ export class EmailVerificationComponent {
         animate: { in: 'fadeIn', out: 'fadeOut' }
         });
       }
+    });
+  }
+
+  resend(){
+    this.verifyLoading = true;
+    this.http.get(ApiService.API_URL+"/email/send_verification", {withCredentials: true, observe: 'response'}).subscribe((response) => {
+      this.verifyLoading = false;
+        superToast({
+          message: "Email sent ! ✅",
+          type: "is-success",
+          dismissible: true,
+        animate: { in: 'fadeIn', out: 'fadeOut' }
+        });
+    }, (error) => {
+      this.verifyLoading = false;
+      superToast({
+        message: "An error occured ! ❌",
+        type: "is-danger",
+        dismissible: true,
+      animate: { in: 'fadeIn', out: 'fadeOut' }
+      });
     });
   }
 
