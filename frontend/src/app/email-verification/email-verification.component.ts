46983a21118415a3127cb30a81d76618b364d2a4
seaweedbrain <nathanstchepinsky@gmail.com>
2023-12-06 12:35:52 -0500

    Add verification with the API and a button to update email

--- frontend/src/app/email-verification/email-verification.component.ts
+++ frontend/src/app/email-verification/email-verification.component.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
-import { faPaperPlane, faArrowRotateLeft } from '@fortawesome/free-solid-svg-icons';
+import { faPaperPlane, faArrowRotateLeft, faPen } from '@fortawesome/free-solid-svg-icons';
+import { UserService } from '../common/User/user.service';
+import { HttpClient } from '@angular/common/http';
+import { Router, ActivatedRoute } from '@angular/router';
+import { ApiService } from '../common/ApiService/api-service';
+import { toast as superToast } from 'bulma-toast'
+
+
 @Component({
   selector: 'app-email-verification',
   templateUrl: './email-verification.component.html',
@@ -8,6 +15,47 @@ import { faPaperPlane, faArrowRotateLeft } from '@fortawesome/free-solid-svg-ico
 export class EmailVerificationComponent {
   faPaperPlane = faPaperPlane;
   faArrowRotateLeft = faArrowRotateLeft;
-  constructor() { }
+  faPen = faPen;
+  emailAddress: string | null = null;
+  code = "";
+  errorMessage = "";
+  constructor(
+    private userService: UserService,
+    private http: HttpClient,
+    private router: Router,
+    private route: ActivatedRoute,
+  ) { 
+    this.emailAddress = this.userService.getEmail();
+  }
+
+
+  verify(){
+    this.errorMessage = '';
+    const data = {
+      "token": this.code
+    }
+    this.http.put(ApiService.API_URL+"/email/verify",  data, {withCredentials: true, observe: 'response'}).subscribe((response) => {
+      if(response.status == 200){
+        superToast({
+          message: "Your email is verified ! ✅",
+          type: "is-success",
+          dismissible: true,
+        animate: { in: 'fadeIn', out: 'fadeOut' }
+        });
+        this.router.navigate(['/login']);
+      }
+    }, (error) => {
+      if(error.status == 403){
+        this.errorMessage = "Invalid or expired code";
+      } else {
+        superToast({
+          message: "An error occured ! ❌",
+          type: "is-danger",
+          dismissible: true,
+        animate: { in: 'fadeIn', out: 'fadeOut' }
+        });
+      }
+    });
+  }
 
 }
