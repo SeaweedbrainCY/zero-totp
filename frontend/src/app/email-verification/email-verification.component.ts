42b184f12b4a9e5daf3931fa49133da9c774b979
seaweedbrain <nathanstchepinsky@gmail.com>
2023-12-06 17:37:14 -0500

    Add support of update email and get role

--- frontend/src/app/email-verification/email-verification.component.ts
+++ frontend/src/app/email-verification/email-verification.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { faPaperPlane, faArrowRotateLeft, faPen } from '@fortawesome/free-solid-svg-icons';
 import { UserService } from '../common/User/user.service';
 import { HttpClient } from '@angular/common/http';
@@ -12,14 +12,18 @@ import { toast as superToast } from 'bulma-toast'
   templateUrl: './email-verification.component.html',
   styleUrls: ['./email-verification.component.css']
 })
-export class EmailVerificationComponent {
+export class EmailVerificationComponent implements OnInit {
   faPaperPlane = faPaperPlane;
   faArrowRotateLeft = faArrowRotateLeft;
   faPen = faPen;
   emailAddress: string | null = null;
   code = "";
   errorMessage = "";
+  emailErrorMessage="";
   verifyLoading = false;
+  emailAddressUpdated="";
+  isEmailModalActive=false;
+  emailLoading  = false;
   constructor(
     private userService: UserService,
     private http: HttpClient,
@@ -30,6 +34,25 @@ export class EmailVerificationComponent {
     if (this.emailAddress == null) {
       this.router.navigate(['/login'], { queryParams: { returnUrl: this.router.url } });
     }
+  }
+
+  ngOnInit(): void {
+    this.http.get(ApiService.API_URL+"/role", {withCredentials: true, observe: 'response'}).subscribe((response) => {
+      try{
+        const user  = JSON.parse(JSON.stringify(response.body));
+        if(user.role != "not_verified"){
+          if(this.userService.getId() == null){
+            this.router.navigate(['/login'], { queryParams: { returnUrl: this.router.url } });
+          } else {
+            this.router.navigate(['/vault'], { queryParams: { returnUrl: this.router.url } });
+          }
+        }
+        } catch {
+          this.router.navigate(['/login'], { queryParams: { returnUrl: this.router.url } });
+        }
+    }, (error) => {
+        this.router.navigate(['/login'], { queryParams: { returnUrl: this.router.url } });
+    });
   }
 
 
@@ -48,7 +71,12 @@ export class EmailVerificationComponent {
           dismissible: true,
         animate: { in: 'fadeIn', out: 'fadeOut' }
         });
-        this.router.navigate(['/login']);
+        if(this.userService.getId() == null){
+          this.router.navigate(['/login'], { queryParams: { returnUrl: this.router.url } });
+        } else {
+          this.router.navigate(['/vault'], { queryParams: { returnUrl: this.router.url } });
+        }
+       
 
       }
     }, (error) => {
@@ -87,4 +115,54 @@ export class EmailVerificationComponent {
     });
   }
 
+  checkEmail(){
+    const forbidden = /["\'<>]/
+    this.emailErrorMessage = "";
+    const emailRegex = /\S+@\S+\.\S+/;
+    if(!emailRegex.test(this.emailAddressUpdated)){
+      this.emailErrorMessage = "Your email is not valid";
+      return false;
+    }
+    if(forbidden.test(this.emailAddressUpdated)){
+      this.emailErrorMessage = "' \" < > characters are forbidden in passwords";
+      return false;
+    }
+    return true;
+  }
+
+  updateEmail(){
+    this.emailErrorMessage = "";
+    const data = {
+      "email": this.emailAddressUpdated
+    }
+      if(this.emailAddressUpdated == ""){
+        this.emailErrorMessage ="Did you forget to fill something ?";
+        return;
+      }
+      if(!this.checkEmail()){
+        return;
+      }
+      this.emailLoading = true;
+      this.http.put(ApiService.API_URL+"/update/email",  data, {withCredentials: true, observe: 'response'}).subscribe((response) => {
+        this.emailLoading = false;
+        superToast({
+          message: "Email updated with success. You should receive a verification email soon ! ✅",
+          type: "is-success",
+          dismissible: true,
+          animate: { in: 'fadeIn', out: 'fadeOut' }
+        });
+        this.userService.setEmail(JSON.parse(JSON.stringify(response.body))["message"])
+        this.emailAddress = this.userService.getEmail();
+        this.isEmailModalActive=false;
+        
+      }, error =>{
+        this.emailLoading = false;
+        if(error.error.message == undefined){
+          error.error.message = "Something went wrong. Please try again later";
+        }
+        this.emailErrorMessage = error.error.message;
+      });
+    }
+
 }
+
