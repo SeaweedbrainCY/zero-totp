31b5556f29f96937cdd07d703663a74999af7d5e
seaweedbrain <nathanstchepinsky@gmail.com>
2023-12-06 11:37:49 -0500

    Add resend code buttin

--- frontend/src/app/email-verification/email-verification.component.ts
+++ frontend/src/app/email-verification/email-verification.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { faPaperPlane } from '@fortawesome/free-solid-svg-icons';
+import { faPaperPlane, faArrowRotateLeft } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-email-verification',
   templateUrl: './email-verification.component.html',
@@ -7,6 +7,7 @@ import { faPaperPlane } from '@fortawesome/free-solid-svg-icons';
 })
 export class EmailVerificationComponent {
   faPaperPlane = faPaperPlane;
+  faArrowRotateLeft = faArrowRotateLeft;
   constructor() { }
 
 }
