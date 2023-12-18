2afde2f753edff3eae17edce916488b0130aed3a
seaweedbrain <nathanstchepinsky@gmail.com>
2023-12-18 14:05:01 -0500

    Add support of email sending for security events

--- api/Email/send.py
+++ api/Email/send.py
@@ -66,3 +66,65 @@ def send_verification_email(email_reciever:str, token:str):# pragma: no cover
             environment.email_sender_address, email_reciever, message.as_string()
         )
 
+def send_information_email(email_reciever:str, reason:str, date:str, ip:str): # pragma: no cover
+    if environment.email_smtp_server == "" or environment.email_smtp_port == "" or environment.email_smtp_username == "" or environment.email_sender_password == "" or environment.email_sender_address == "":
+        logging.error("Email configuration is not set. Please set the email configuration in the environment file.")
+        raise Exception("Email configuration is not set. Please set the email configuration in the environment file.")
+    message = MIMEMultipart("alternative")
+    message["Subject"] = reason
+    message["From"] = environment.email_sender_address
+    message["To"] = email_reciever
+
+    text = f"""\
+        Hi, 
+        We wanted to inform you that {reason}.
+
+        Date: {date}
+        IP: {ip}
+
+        If you don't recognize this activity, please change your password immediately and contact us. 
+
+        This is a security notification from Zero-TOTP. For safety reasons, you can't unsubscribe from these emails.
+
+        Have a safe and secure Zero-Trust journey!
+
+        The Zero-TOTP Team
+    """
+    html = f"""\
+<!DOCTYPE html>
+<html>
+    <body style="font-family: 'Arial', sans-serif; color: #16262E;">
+
+    <div style="max-width: 600px; margin: 0 auto; padding: 20px; border: 1px solid #5AA9E6; border-radius: 10px;; color: #16262E;">
+        <h2 style="color: #5AA9E6; ">Hi,</h2>
+        <p>We wanted to inform you that <strong><span style="color: #FE6847;">{reason}</span></strong></p>
+
+        <ul>
+            <li>Date: {date}</li>
+            <li>IP: {ip}</li>
+        </ul>
+
+        <p><strong>If you don't recognize this activity, please change your password immediately and contact us.</strong></p>
+
+        <p>This is a security notification from Zero-TOTP. For safety reasons, you can't unsubscribe from these emails.</p>
+
+        <p>Have a safe and secure Zero-Trust journey!</p>
+
+        <p>Best regards,<br> The Zero-TOTP Team</p>
+    </div>
+
+</body>
+</html>
+    """
+    part1 = MIMEText(text, "plain", "utf-8")
+    part2 = MIMEText(html, "html", "utf-8")
+
+    message.attach(part1)
+    message.attach(part2)
+    with smtplib.SMTP(environment.email_smtp_server, environment.email_smtp_port) as server:
+        server.connect(environment.email_smtp_server, environment.email_smtp_port)
+        server.starttls()
+        server.login(environment.email_smtp_username, environment.email_sender_password)
+        server.sendmail(
+            environment.email_sender_address, email_reciever, message.as_string()
+        )
