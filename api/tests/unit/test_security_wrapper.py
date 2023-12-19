5fc3f272e9b61b1ada11974ad3d79e570ec7d844
seaweedbrain <nathanstchepinsky@gmail.com>
2023-12-19 14:56:33 -0500

    Add tests for blocked users

--- api/tests/unit/test_security_wrapper.py
+++ api/tests/unit/test_security_wrapper.py
@@ -11,14 +11,17 @@ class TestSecurityWrapper(unittest.TestCase):
         def setUp(self):
             self.not_verified_user_id = 1
             self.verified_user_id = 2
+            self.blocked_user_id = 3
             self.flask_application = app
     
             not_verified_user = UserModel(id=self.not_verified_user_id,username="user", mail="user@user.com", password="pass", derivedKeySalt="AAA", isVerified = False, passphraseSalt = "AAAA", createdAt="01/01/2001")
             verified_user_id = UserModel(id=self.verified_user_id,username="user", mail="user@user.com", password="pass", derivedKeySalt="AAA", isVerified = True, passphraseSalt = "AAAA", createdAt="01/01/2001")
+            blocked_user_id = UserModel(id=self.blocked_user_id,username="user", mail="user@user.com", password="pass", derivedKeySalt="AAA", isVerified = True, passphraseSalt = "AAAA", createdAt="01/01/2001", isBlocked = True)
             with self.flask_application.app.app_context():
                 db.create_all()
                 db.session.add(not_verified_user)
                 db.session.add(verified_user_id)
+                db.session.add(blocked_user_id)
                 db.session.commit()
         
         def tearDown(self):
@@ -65,4 +68,20 @@ class TestSecurityWrapper(unittest.TestCase):
                 _, status = wrapped_function({"user": self.verified_user_id},self.verified_user_id, {"user": self.verified_user_id})
                 self.assertEqual(status, 200)
 
-             
+        
+        def test_valid_user_blocked(self):
+            @security_wrapper.require_valid_user
+            def wrapped_function(user_id):
+                return True, 200
+            with self.flask_application.app.app_context():
+                _, status = wrapped_function({"user": self.blocked_user_id},self.blocked_user_id, {"user": self.blocked_user_id})
+                self.assertEqual(status, 403)
+
+        def test_require_userid_blocked(self):
+            security_wrapper.require_email_validation = True
+            @security_wrapper.require_userid
+            def wrapped_function(user_id):
+                return True, 200
+            with self.flask_application.app.app_context():
+                _, status = wrapped_function({"user": self.blocked_user_id},self.blocked_user_id, {"user": self.blocked_user_id})
+                self.assertEqual(status, 403)
