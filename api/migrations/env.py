2cb4a3706d4ae901f2aaf947e5db3b394f7699df
seaweedbrain <nathanstchepinsky@gmail.com>
2024-01-22 16:17:22 -0500

    Add model to alembic

--- api/migrations/env.py
+++ api/migrations/env.py
@@ -2,7 +2,7 @@ import logging
 from logging.config import fileConfig
 
 from flask import current_app
-
+from database import model
 from alembic import context
 
 # this is the Alembic Config object, which provides
