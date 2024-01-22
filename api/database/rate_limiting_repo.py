7a59c2109f9baa5774622a0fccf1f7144c5e58fe
seaweedbrain <nathanstchepinsky@gmail.com>
2024-01-22 16:49:35 -0500

    fix typo

--- api/database/rate_limiting_repo.py
+++ api/database/rate_limiting_repo.py
@@ -4,27 +4,27 @@ import datetime
 
 class RateLimitingRepo:
     def add_failed_login(self, ip, user_id):
-        rl = RateLimiting(ip=ip, user_id=user_id, action_type="failed_login", timestramp= datetime.datetime.utcnow())
+        rl = RateLimiting(ip=ip, user_id=user_id, action_type="failed_login", timestamp= datetime.datetime.utcnow())
         db.session.add(rl)
         db.session.commit()
         return rl
     
     def add_send_verification_email(self, ip, user_id):
-        rl = RateLimiting(ip=ip, user_id=user_id, action_type="send_verification_email", timestramp= datetime.datetime.utcnow())
+        rl = RateLimiting(ip=ip, user_id=user_id, action_type="send_verification_email", timestamp= datetime.datetime.utcnow())
         db.session.add(rl)
         db.session.commit()
         return rl
     
     def is_login_rate_limited (self, ip):
         time_period = datetime.datetime.utcnow() - datetime.timedelta(minutes=60) 
-        rl = db.session.query(RateLimiting).filter_by(ip=ip, action_type="failed_login").filter(RateLimiting.timestramp > time_period).all()
+        rl = db.session.query(RateLimiting).filter_by(ip=ip, action_type="failed_login").filter(RateLimiting.timestamp > time_period).all()
         if len(rl) >= 10:
             return True
         return False
     
     def is_send_verification_email_rate_limited (self, user_id):
         time_period = datetime.datetime.utcnow() - datetime.timedelta(minutes=60) 
-        rl = db.session.query(RateLimiting).filter_by(user_id=user_id, action_type="send_verification_email").filter(RateLimiting.timestramp > time_period).all()
+        rl = db.session.query(RateLimiting).filter_by(user_id=user_id, action_type="send_verification_email").filter(RateLimiting.timestamp > time_period).all()
         if len(rl) >= 5:
             return True
         return False
