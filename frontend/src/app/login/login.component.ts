4afea9d69fce9c265ea694dc6ac26dd9201c334f
seaweedbrain <nathanstchepinsky@gmail.com>
2024-01-23 15:59:47 -0500

    fix missing param

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -386,7 +386,8 @@ export class LoginComponent implements OnInit {
       }
     }, error => {
       if(error.status == 429){
-        this.translate.get("login.errors.rate_limited").subscribe((translation)=>{
+        const ban_time = error.error.ban_time || "few";
+        this.translate.get("login.errors.rate_limited", {time:String(ban_time)} ).subscribe((translation)=>{
         superToast({
           message: translation,
           type: "is-danger",
