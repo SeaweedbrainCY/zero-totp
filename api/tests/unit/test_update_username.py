62d8a8259ac08539c8e38575ef1185e98420ca38
seaweedbrain <nathanstchepinsky@gmail.com>
2024-04-05 18:02:15 -0400

    Fix typo

--- api/tests/unit/test_update_username.py
+++ api/tests/unit/test_update_username.py
@@ -69,7 +69,7 @@ class TestUpdateUsername(unittest.TestCase):
             self.assertEqual(response.status_code, 403)
             self.assertEqual(response.json(), {"error": "Not verified"})
      
-     def test_update_username_too_long(self):
+    gdef test_update_username_too_long(self):
         with self.flask_application.app.app_context():
             self.client.cookies = {"api-key": generate_jwt(self.user1_id)}
             response = self.client.put(self.endpoint, json={"username": "a"*321})
