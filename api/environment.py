fbc7c3f1ef2a1c885c59671cc635c661836f239e
Nathan <nathanstchepinsky@gmail.com>
2024-05-02 01:30:18 +0000

    Improve config parsing with struct

--- api/environment.py
+++ api/environment.py
@@ -4,6 +4,103 @@ import pyyaml
 
 # TODO : add requirements for each value
 
+class EnvironmentConfig:
+    def __init__(self, env) -> None:
+        if env == "local":
+            self.type = "local"
+            logging.basicConfig(
+                format='%(asctime)s %(levelname)-8s %(message)s',
+                level=logging.DEBUG,
+                datefmt='%d-%m-%Y %H:%M:%S')
+            logging.debug("Environment set to development")
+            self.frontend_domain = 'zero-totp.local'
+            self.frontend_URI = ['http://localhost:4200']
+            self.callback_URI = 'http://localhost:8080/google-drive/oauth/callback'
+            os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+        elif env == "development":
+            self.type = "development"
+            logging.basicConfig(
+                format='%(asctime)s %(levelname)-8s %(message)s',
+                level=logging.INFO,
+                datefmt='%d-%m-%Y %H:%M:%S')
+            logging.info("Environment set to development")
+            self.frontend_domain = 'dev.zero-totp.com'
+            self.frontend_URI = ['https://dev.zero-totp.com']
+            self.callback_URI = "https://dev.zero-totp.com/google-drive/oauth/callback"
+        else:
+            self.type = "production"
+            logging.basicConfig(
+                filename="/var/log/api/api.log",
+                filemode='a',
+                format='%(asctime)s %(levelname)-8s %(message)s',
+                level=logging.INFO,
+                datefmt='%d-%m-%Y %H:%M:%S')
+            self.frontend_domain="zero-totp.com"
+            self.frontend_URI = ["https://zero-totp.com", "https://ca.zero-totp.com", "https://eu.zero-totp.com"]
+            self.callback_URI = "https://api.zero-totp.com/google-drive/oauth/callback"
+
+class OauthConfig:
+    required_keys = ["client_secret_file_path"]
+
+    def __init__(self, data):
+        for key in self.required_keys:
+            if key not in data:
+                logging.error(f"Was expecting the key api.oauth.'{key}'")
+                raise Exception(f"Was expecting the key api.oauth.'{key}'")
+        self.client_secret_file_path = data["client_secret_file_path"]
+
+class APIConfig:
+    required_keys = ["database_uri", "jwt_secret", "private_key_path", "public_key_path", "flask_secret_key", "sever_side_encryption_key"]
+    option_config = ["oauth"]
+
+    def __init__(self, data):
+        for key in self.required_keys:
+            if key not in data:
+                logging.error(f"Was expecting the key api.'{key}'")
+                raise Exception(f"Was expecting the key api.'{key}'")
+        for key in self.option_config:
+            if key not in data:
+                logging.warning(f"api.'{key}' is not set. Ignoring it ...")
+        if "port" not in data:
+            logging.warning(f"api.'port' is not set. Using default value: 8080")
+            data["port"] = 8080
+        
+        try:
+            self.port = int(data["port"]) 
+        except:
+            logging.warning("api.port is not valid. Ignoring it. Setting default value: 8080")
+            self.port = 8080
+        self.database_uri = data["database_uri"]
+        self.jwt_secret = data["jwt_secret"]
+        self.private_key_path = data["private_key_path"]
+        self.public_key_path = data["public_key_path"]
+        self.flask_secret_key = data["flask_secret_key"]
+        self.sever_side_encryption_key = data["sever_side_encryption_key"]
+        if "oauth" in data:
+            self.oauth = OauthConfig(data["oauth"])
+        else:
+            self.oauth = None
+        
+class DatabaseConfig:
+    required_keys = ["uri"]
+    def __init__(self, data):
+        for key in self.required_keys:
+            if key not in data:
+                logging.error(f"Was expecting the key database.'{key}'")
+                raise Exception(f"Was expecting the key database.'{key}'")
+        self.uri = data["uri"]
+
+class Config:
+    required_keys = ["api", "environment", "database"]
+    def __init__(self, data):
+        for key in self.required_keys:
+            if key not in data:
+                raise Exception(f"Was expecting the key '{key}'")
+        self.api = APIConfig(data["api"])
+        self.environment = EnvironmentConfig(data["environment"])
+        self.database = DatabaseConfig(data["database"])
+
+
 
 
 
@@ -16,76 +113,15 @@ with open("./config/config.yml") as config_yml:
         raise Exception(exc)
 
 
-def parse_conf(conf):
-    ##API 
-    if "api" in conf:
-        global port
-        port = None 
-        if "port" in conf["api"]:
-            try :
-                port =  int(conf["api"]["port"])
-            except:
-                logging.warning("api.port is not valid. Ignoring it ...")
-        if port == None:
-            port = 8080
-            logging.warning("api.port not set. Using default value: 8080")
-        
-        if "database_uri" in conf["api"]:
-            global db_uri
-            db_uri = conf["api"]["database_uri"]
-        else:
-            logging.error("api.database_uri is not set. Please set it to a valid database URI. Aborting...")
-            raise Exception("api.database_uri is not set. Please set it to a valid database URI. ")
-
-        if "jwt_secret" in conf["api"]:
-            global jwt_secret 
-            jwt_secret = conf["api"]["jwt_secret"]
-        else:
-            logging.error("api.jwt_secret is not set. Please set  a valid secret key. Aborting...")
-            raise Exception("api.jwt_secret is not set. Please set  a valid secret key.")
-
-        if "private_key_path" in conf["api"]:
-            global private_key_path
-            private_key_path = conf["api"]["private_key_path"]
-        else:
-            logging.error("api.private_key_path is not set. Please set  a valid private_key_path path. Aborting...")
-            raise Exception("api.private_key_path is not set. Please set  a valid private_key_path path.")
-
-        if "public_key_path" in conf["api"]:
-            global public_key_path
-            public_key_path = conf["api"]["public_key_path"]
-        else:
-            logging.error("api.public_key_path is not set. Please set  a valid public_key_path path. Aborting...")
-            raise Exception("api.public_key_path is not set. Please set  a valid public_key_path path.")
-        
-        if "flask_secret_key" in conf["api"]:
-            flask_secret_key = conf["api"]["flask_secret_key"]
-        else:
-            logging.error("FLASK_SECRET_KEY environment variable not set. Please set it to a valid secret key. Aborting...")
-            raise Exception("FLASK_SECRET_KEY environment variable not set. Please set it to a valid secret key.")
-
-        if "oauth" in conf["api"]:
-            if "client_secret_file_path" in conf["api"]["oauth"]:
-                oauth_client_secret_file = conf["api"]["oauth"]["client_secret_file_path"]
-            else :
-                logging.error("api.oauth is set but no client_secret_file_path path set. Please set it to a valid path to the client_secret.json file. Aborting...")
-                raise Exception("api.oauth is set but no client_secret_file_path path set. Please set it to a valid path to the client_secret.json file.")
-        else:
-            logging.warning("api.oauth is empty. No oauth configuration provided. Oauth will not be supported.")
-
-    else:
-        raise Exception("Was expecting the key 'api'") 
-
-
 #port = int(os.environ.get('PORT')) if os.environ.get('PORT') != None else None
 #db_uri = os.environ.get('DATABASE_URI')
-environment = os.environ.get('ENVIRONMENT')
+#environment = os.environ.get('ENVIRONMENT')
 #jwt_secret = os.environ.get('JWT_SECRET')
 #private_key_path = os.environ.get('PRIVATE_KEY_PATH')
 #public_key_path = os.environ.get('PUBLIC_KEY_PATH')
 #oauth_client_secret_file = os.environ.get('OAUTH_CLIENT_SECRET_FILE')
 #flask_secret_key = os.environ.get('FLASK_SECRET_KEY')
-sever_side_encryption_key = os.environ.get('SEVER_SIDE_ENCRYPTION_KEY')
+#sever_side_encryption_key = os.environ.get('SEVER_SIDE_ENCRYPTION_KEY')
 are_all_tables_created = False
 admin_can_delete_users = os.environ.get('ADMIN_CAN_DELETE_USERS') == "true"
 email_sender_address = os.environ.get('EMAIL_SENDER_ADDRESS')
@@ -99,38 +135,6 @@ login_attempts_limit_per_ip = int(os.environ.get('LOGIN_ATTEMPTS_LIMIT_PER_IP'))
 send_email_attempts_limit_per_user = int(os.environ.get('SEND_EMAIL_ATTEMPTS_LIMIT_PER_USER')) if os.environ.get('SEND_EMAIL_ATTEMPTS_LIMIT_PER_USER') != None else 5
 login_ban_time = int(os.environ.get('LOGIN_BAN_TIME')) if os.environ.get('LOGIN_BAN_TIME') != None else 15
 email_ban_time = int(os.environ.get('EMAIL_BAN_TIME')) if os.environ.get('EMAIL_BAN_TIME') != None else 60
-
-
-if environment == "local":
-    logging.basicConfig(
-        format='%(asctime)s %(levelname)-8s %(message)s',
-        level=logging.DEBUG,
-        datefmt='%d-%m-%Y %H:%M:%S')
-    logging.debug("Environment set to development")
-    frontend_domain = 'zero-totp.local'
-    frontend_URI = ['http://localhost:4200']
-    callback_URI = 'http://localhost:8080/google-drive/oauth/callback'
-    os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
-elif environment == "development":
-    logging.basicConfig(
-        format='%(asctime)s %(levelname)-8s %(message)s',
-        level=logging.INFO,
-        datefmt='%d-%m-%Y %H:%M:%S')
-    logging.info("Environment set to development")
-    frontend_domain = 'dev.zero-totp.com'
-    frontend_URI = ['https://dev.zero-totp.com']
-    callback_URI = "https://dev.zero-totp.com/google-drive/oauth/callback"
-else:
-    environment = "production"
-    logging.basicConfig(
-        filename="/var/log/api/api.log",
-        filemode='a',
-        format='%(asctime)s %(levelname)-8s %(message)s',
-        level=logging.INFO,
-        datefmt='%d-%m-%Y %H:%M:%S')
-    frontend_domain="zero-totp.com"
-    frontend_URI = ["https://zero-totp.com", "https://ca.zero-totp.com", "https://eu.zero-totp.com"]
-    callback_URI = "https://api.zero-totp.com/google-drive/oauth/callback"
 
 
 
