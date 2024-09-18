c6ce66fdd77a8b631ce0547566f9e7980132398f
seaweedbrain <nathanstchepinsky@gmail.com>
2024-09-18 11:20:14 -0400

    Better handling of the different cause of start crash

--- api/start.sh
+++ api/start.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env bash
-alembic check 
-if [ $? -ne 0 ]; then
-    echo "❌  Your database is not up to date. Follow Zero-TOTP's documentation to update it."
-    echo "📚 https://docs.zero-totp.com"
-    tail -f /dev/null
+alembic check > /tmp/alembic_check.log 2>&1
+if [ $? -eq 0 ]; then 
+    cat /tmp/alembic_check.log > /var/log/api/api.log
+    echo "🎉  Your database is up to date."
+elif cat /tmp/alembic_check.log | grep "Target database is not up to date."; then
+    cat /tmp/alembic_check.log
+    echo "🚨  Your database is not up to date. Upgrading..."
+    alembic upgrade head
+else
+    cat /tmp/alembic_check.log
+    echo "❌  An error occurred while checking the database. The connection is very likely to be broken or impossible. Please check the above logs. As it can be a temporary issue, Docker will restart now to try again."
+    exit 1
 fi
 echo "🍺  All logs are in /var/log/api"
 echo "🚀  Starting gunicorn"
