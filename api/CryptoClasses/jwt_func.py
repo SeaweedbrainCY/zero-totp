67b1d4329d71c6c84ed791472084e3490a83f35c
seaweedbrain <nathanstchepinsky@gmail.com>
2024-10-06 12:55:03 -0400

    Add JTI in JWT and return it

--- api/CryptoClasses/jwt_func.py
+++ api/CryptoClasses/jwt_func.py
@@ -9,7 +9,7 @@ import logging
 from connexion.exceptions import Forbidden
 
 ALG = 'HS256'
-ISSUER = "https://api.zero-totp.com"
+ISSUER = "https://zero-totp.com/api/v1"
 
 # Verification performed by openAPI
 def verify_jwt(jwt_token): 
@@ -33,9 +33,11 @@ def verify_jwt(jwt_token):
 
 def generate_jwt(user_id, admin=False):
     try:
+        jti = str(uuid.uuid4())
         payload = {
             "iss": ISSUER,
             "sub": user_id,
+            "jti": jti,
             "iat": datetime.datetime.utcnow(),
             "nbf": datetime.datetime.utcnow(),
             "exp": datetime.datetime.utcnow() + datetime.timedelta(hours=1),
@@ -43,7 +45,7 @@ def generate_jwt(user_id, admin=False):
         if admin:
             payload["admin"] = True
             payload["exp"] = datetime.datetime.utcnow() + datetime.timedelta(minutes=10)
-        return jwt.encode(payload, conf.api.jwt_secret, algorithm=ALG)
+        return jwt.encode(payload, conf.api.jwt_secret, algorithm=ALG), jti
     except Exception as e:
         logging.warning("Error while generating JWT : " + str(e))
         raise e
