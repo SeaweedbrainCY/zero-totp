93fc6aeb30f3889fbc6d5c6111cbd269b876f0a1
seaweedbrain <nathanstchepinsky@gmail.com>
2024-10-22 22:17:17 -0400

    Improve CSP header and nginx.conf generation

--- frontend/generate_CSP_hashes.sh
+++ frontend/generate_CSP_hashes.sh
@@ -2,19 +2,18 @@
 
 hashes=$(grep -oE 'sha384-[a-zA-Z0-9+/=]*' dist/frontend/browser/index.html |  sed "s/^/'/" | sed "s/\$/'/" | tr '\n' ' ')
 
-echo "
-events{}
+echo "events{}
 http {
     include /etc/nginx/mime.types;
     server {
         listen 80;
         root /usr/share/nginx/html/frontend/browser;
         index index.html;
-        add_header Content-Security-Policy \"default-src https://*.zero-totp.com; style-src 'self' 'unsafe-inline'; object-src 'none' ; script-src $hashes ; img-src 'self' https://icons.duckduckgo.com/; base-uri 'self'; frame-src 'self'; connect-src 'self' https://*.zero-totp.com; manifest-src 'self'\" always;
+        add_header Content-Security-Policy \"default-src https://*.zero-totp.com; style-src 'self' 'unsafe-inline'; object-src 'none' ; script-src $hashes; img-src 'self' https://icons.duckduckgo.com/; base-uri 'self'; frame-src 'self'; connect-src 'self' https://*.zero-totp.com; manifest-src 'self'; worker-src 'self';\" always;
 
         location / {
-            try_files $uri $uri/ /index.html;
+            try_files \$uri \$uri/ /index.html;
         }
     }
 }
-"
+" > nginx.conf
