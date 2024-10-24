e60c4156196a158dab6c279c077a8236b2d7fa42
seaweedbrain <nathanstchepinsky@gmail.com>
2024-10-24 00:28:10 -0400

    Add repo for get,revoke and refresh a refresh token

--- api/database/refresh_token_repo.py
+++ api/database/refresh_token_repo.py
@@ -7,10 +7,20 @@ from uuid import uuid4
 
 class RefreshTokenRepo:
 
-    def create_refresh_token(self, user_id, jti, hashed_token):
-        expiration_timestamp = datetime.datetime.utcnow() + datetime.timedelta(seconds=conf.api.refresh_token_valid)
-        id = str(uuid4())
+    def create_refresh_token(self, user_id, jti, hashed_token, expiration=-1):
+        expiration_timestamp = (datetime.datetime.now(datetime.UTC) + datetime.timedelta(seconds=conf.api.refresh_token_valid)).timestamp()
+        id = str(uuid4()) if expiration == -1 else expiration
         rt = RefreshToken(id=id, user_id=user_id, jti=jti, hashed_token=hashed_token, expiration=expiration_timestamp)
         db.session.add(rt)
+        db.session.commit()
+        return rt
+    
+
+    def get_refresh_token_by_hash(self, hashed_token):
+        return RefreshToken.query.filter_by(hashed_token=hashed_token).first()
+    
+    def revoke(self, id):
+        rt = RefreshToken.query.filter_by(id=id).first()
+        rt.revoke_timestamp = datetime.datetime.now(datetime.UTC).timestamp()
         db.session.commit()
         return rt
