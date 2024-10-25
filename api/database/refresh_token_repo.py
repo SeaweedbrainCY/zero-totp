aeeb39932737f4ce9dab02e29a0dfc0cc630bf78
seaweedbrain <nathanstchepinsky@gmail.com>
2024-10-25 17:43:46 -0400

    Fix typo

--- api/database/refresh_token_repo.py
+++ api/database/refresh_token_repo.py
@@ -8,7 +8,7 @@ from uuid import uuid4
 class RefreshTokenRepo:
 
     def create_refresh_token(self, user_id, jti, hashed_token, expiration=-1):
-        expiration_timestamp = (datetime.datetime.now(datetime.UTC) + datetime.timedelta(seconds=conf.api.refresh_token_valid)).timestamp()
+        expiration_timestamp = (datetime.datetime.now(datetime.UTC) + datetime.timedelta(seconds=conf.api.refresh_token_validity)).timestamp()
         id = str(uuid4()) if expiration == -1 else expiration
         rt = RefreshToken(id=id, user_id=user_id, jti=jti, hashed_token=hashed_token, expiration=expiration_timestamp)
         db.session.add(rt)
