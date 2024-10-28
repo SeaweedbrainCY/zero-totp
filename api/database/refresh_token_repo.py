4d3da0ec08f01e835b12dfebf55248ecf069fdc5
seaweedbrain <nathanstchepinsky@gmail.com>
2024-10-27 22:11:06 -0400

    Fix typo causing the expiration to not follow the given one

--- api/database/refresh_token_repo.py
+++ api/database/refresh_token_repo.py
@@ -8,8 +8,8 @@ from uuid import uuid4
 class RefreshTokenRepo:
 
     def create_refresh_token(self, user_id, jti, hashed_token, expiration=-1):
-        expiration_timestamp = (datetime.datetime.now(datetime.UTC) + datetime.timedelta(seconds=conf.api.refresh_token_validity)).timestamp()
-        id = str(uuid4()) if expiration == -1 else expiration
+        expiration_timestamp = (datetime.datetime.now(datetime.UTC) + datetime.timedelta(seconds=conf.api.refresh_token_validity)).timestamp() if expiration == -1 else expiration
+        id = str(uuid4()) 
         rt = RefreshToken(id=id, user_id=user_id, jti=jti, hashed_token=hashed_token, expiration=expiration_timestamp)
         db.session.add(rt)
         db.session.commit()
