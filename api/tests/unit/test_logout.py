e25846ae35d0810aa479b69eaa2f2fb483c34234
seaweedbrain <nathanstchepinsky@gmail.com>
2024-11-17 16:11:39 -0500

    Fix tests

--- api/tests/unit/test_logout.py
+++ api/tests/unit/test_logout.py
@@ -25,7 +25,7 @@ class TestLogout(unittest.TestCase):
         self.user_id = 1
         self.jwt_token = generate_jwt(self.user_id)
         with self.flask_application.app.app_context():
-            user = UserModel(id=self.user_id,username="user", mail="user@user.com", password="pass", derivedKeySalt="AAA", isVerified = False, passphraseSalt = "AAAA", createdAt="01/01/2001")
+            user = UserModel(id=self.user_id,username="user", mail="user@user.com", password="pass", derivedKeySalt="AAA", isVerified = True, passphraseSalt = "AAAA", createdAt="01/01/2001")
             db.create_all()
             db.session.add(user)
             db.session.commit()
@@ -42,10 +42,27 @@ class TestLogout(unittest.TestCase):
             db.drop_all()
             patch.stopall()
     
+    def generate_jwt_expired(self, user_id):
+        jti = str(uuid4())
+        try:
+            payload = {
+                "iss": ISSUER,
+                "sub": user_id,
+                "iat": datetime.datetime.now(datetime.UTC),
+                "nbf": datetime.datetime.now(datetime.UTC),
+                "exp": datetime.datetime.now(datetime.UTC) - datetime.timedelta(seconds=conf.api.access_token_validity),
+                "jti": jti
+            }
+            return jwt.encode(payload, conf.api.jwt_secret, algorithm=ALG), jti
+        except Exception as e:
+            logging.warning("Error while generating JWT : " + str(e))
+            raise e
+    
     def test_logout(self):
         with self.flask_application.app.app_context():
             self.client.cookies = {"api-key": self.jwt_token}
             response = self.client.put(self.endpoint)
+            print(response.json())
             self.assertEqual(response.status_code, 200)
             self.assertIsNotNone(RefreshTokenRepo().get_refresh_token_by_jti(self.jti).revoke_timestamp)
 
@@ -54,6 +71,5 @@ class TestLogout(unittest.TestCase):
         with self.flask_application.app.app_context():
             response = self.client.put(self.endpoint)
             self.assertEqual(response.status_code, 401)
-            self.assertIsNone(RefreshTokenRepo().get_refresh_token_by_jti(self.jti).revoke_timestamp)
 
 
