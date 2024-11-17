7e15114f70fc7766141fb93201792600902f11fd
seaweedbrain <nathanstchepinsky@gmail.com>
2024-11-17 18:53:56 -0500

    Set session token instead of JWT

--- api/Utils/http_response.py
+++ api/Utils/http_response.py
@@ -2,6 +2,6 @@ from flask import Response
 from environment import conf
 
 class Response(Response):
-    def set_auth_cookies(self, jwt, refresh_token):
-        self.set_cookie("api-key", jwt, httponly=True, secure=True, samesite="Lax", max_age=conf.api.refresh_token_validity, path="/api/")
+    def set_auth_cookies(self, session_token, refresh_token):
+        self.set_cookie("session-token", session_token, httponly=True, secure=True, samesite="Lax", max_age=conf.api.refresh_token_validity, path="/api/")
         self.set_cookie("refresh-token", refresh_token, httponly=True, secure=True, samesite="Lax", max_age=conf.api.refresh_token_validity, path="/api/v1/auth/refresh")
