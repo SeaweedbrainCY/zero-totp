093160aa33a31698121d4360e50674bd7c77953e
seaweedbrain <nathanstchepinsky@gmail.com>
2024-11-17 23:36:35 -0500

    Raise specific errors to be catch by the frontend and init refresh flow

--- api/CryptoClasses/session_verification.py
+++ api/CryptoClasses/session_verification.py
@@ -1,24 +1,23 @@
 from database.session_token_repo import SessionTokenRepo
 from flask import request
 from app import app
-from connexion.exceptions import OAuthProblem
+from connexion.exceptions import Unauthorized, Forbidden
 from hashlib import sha256
 from environment import logging
 from Utils.utils import get_ip
 import datetime as dt
 
 def verify_session(token_info):
-    remote_ip = get_ip(request)
     if not token_info:
-        raise OAuthProblem("No session token provided")
+        raise Unauthorized("No session token provided")
     with app.app.app_context():
-        session_token = SessionTokenRepo().get_session_token(token_info["session-token"])
-    if not session_token:
-        raise OAuthProblem("Invalid session token")
-    if session_token.expiration < dt.datetime.now(dt.UTC).timestamp():
-        logging.info(f"Rejected session token {session_token.id} because of expiration. User {session_token.user_id} from IP {remote_ip}")
-        raise OAuthProblem("Invalid session token")
-    if session_token.revoke_timestamp is not None:
-        logging.info(f"Rejected session token {session_token.id} because it was revoked. User {session_token.user_id} from IP {remote_ip}")
-        raise OAuthProblem("Invalid session token")
-    return  {"uid" : session_token.user_id}
+        remote_ip = get_ip(request)
+        session_token = SessionTokenRepo().get_session_token(token_info)
+        if not session_token:
+            raise Unauthorized("Invalid session token")
+        if float(session_token.expiration) < dt.datetime.now(dt.UTC).timestamp():
+            raise Unauthorized("API key expired")
+        if session_token.revoke_timestamp is not None:
+            logging.info(f"Rejected session token {session_token.id} because it was revoked. User {session_token.user_id} from IP {remote_ip}")
+            raise Forbidden("Invalid session token")
+        return  {"uid" : session_token.user_id}
