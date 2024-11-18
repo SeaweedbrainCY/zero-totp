c736c1d65843b960106179526f6f069124cbb40f
seaweedbrain <nathanstchepinsky@gmail.com>
2024-11-17 22:51:29 -0500

    Remove admin repo

--- api/database/admin_repo.py
+++
@@ -1,6 +0,0 @@
-from database.db import db 
-from zero_totp_db_model.model import Admin as Admin_model
-
-class Admin:
-   def get_by_user_id(self, user_id):
-        return db.session.query(Admin_model).filter_by(user_id=user_id).first()
