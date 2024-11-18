0d06376968c461d6aa93318deddac321ebd7ad95
seaweedbrain <nathanstchepinsky@gmail.com>
2024-11-17 23:35:54 -0500

    Rewrite the whole refresh flow with session tokens

--- api/CryptoClasses/refresh_token.py
+++ api/CryptoClasses/refresh_token.py
@@ -3,7 +3,6 @@ from hashlib import sha256
 from database.refresh_token_repo import RefreshTokenRepo
 from environment import logging
 import datetime as dt
-from CryptoClasses.jwt_func import generate_jwt, verify_jwt
 from connexion.exceptions import Forbidden, Unauthorized
 from database.rate_limiting_repo import RateLimitingRepo
 from database.session_token_repo import SessionTokenRepo
@@ -18,30 +17,31 @@ def generate_refresh_token(user_id, session_token_id, expiration=-1):
     return token if rt else None
     
     
-def refresh_token_flow(session_token_id, rt, jwt_user_id, ip):
+def refresh_token_flow(refresh, session, ip):
         rate_limiting = RateLimitingRepo()
         rt_repo = RefreshTokenRepo()
         session_repo = SessionTokenRepo()
-        if rt.session_token_id == session_token_id and rt.user_id == jwt_user_id:
-            if rt.revoke_timestamp == None:
-                if float(rt.expiration) > dt.datetime.now(dt.UTC).timestamp():
-                    new_jwt = generate_jwt(rt.user_id)
-                    new_jti = verify_jwt(new_jwt)["jti"]
-                    new_refresh_token = generate_refresh_token(rt.user_id, new_jti, expiration=rt.expiration)
-                    rt_repo.revoke(rt.id)
-                    return new_jwt, new_refresh_token
+        if refresh.session_token_id == session.id and refresh.user_id == session.user_id:
+            if refresh.revoke_timestamp == None:
+                if float(refresh.expiration) > dt.datetime.now(dt.UTC).timestamp():
+                    new_session_id, new_session_token = session_repo.generate_session_token(session.user_id)
+                    new_refresh_token = generate_refresh_token(session.user_id, new_session_id, expiration=refresh.expiration)
+                    rt_repo.revoke(refresh.id)
+                    session_repo.revoke(session.id)
+                    return new_session_token, new_refresh_token
                 else:
-                    rate_limiting.add_failed_login(ip, rt.user_id)
-                    logging.warning(f"The user {rt.user_id} tried to refresh a token that has expired: {rt.id}. Refresh flow aborted. Token expired at: {rt.expiration}")
+                    rate_limiting.add_failed_login(ip, refresh.user_id)
+                    logging.warning(f"The user {refresh.user_id} tried to refresh a token that has expired: {refresh.id}. Refresh flow aborted. Token expired at: {refresh.expiration}")
                     raise Forbidden("Access denied")
             else:
-                rate_limiting.add_failed_login(ip, rt.user_id)
-                logging.warning(f"The user {rt.user_id} tried to refresh a token that has been revoked: {rt.id}. Refresh flow aborted. Token revoked at: {rt.revoke_timestamp}")
+                rate_limiting.add_failed_login(ip, refresh.user_id)
+                logging.warning(f"The user {refresh.user_id} tried to refresh a token that has been revoked: {refresh.id}. Refresh flow aborted. Token revoked at: {refresh.revoke_timestamp}")
                 raise Forbidden("Access denied")
         else:
-            rate_limiting.add_failed_login(ip, rt.user_id)
-            logging.warning(f"A refresh token has been asked, but invalid context for refresh token {rt.id}. Expected jti: {rt.jti}, user_id: {rt.user_id}. But git jti: {jti}, user_id: {jwt_user_id}. Refresh flow aborted.")
-            rt_repo.revoke(rt.id)
+            rate_limiting.add_failed_login(ip, session.user_id)
+            logging.warning(f"A refresh token has been asked, but invalid context for refresh token {refresh.id}. Session : Id : {session.id}, User {session.user_id}. Refresh : Id : {refresh.id}, User: {refresh.user_id}, Association session id : {refresh.session_token_id}. Refresh flow aborted.")
+            rt_repo.revoke(refresh.id)
+            session_repo.revoke(session.id)
             raise Forbidden("Access denied")
 
 
