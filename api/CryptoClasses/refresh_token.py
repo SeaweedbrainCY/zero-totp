4b09b3ca3110fcedbfe885d5117afbdc66bab1d0
seaweedbrain <nathanstchepinsky@gmail.com>
2024-11-21 07:21:16 -0500

    Use new revoke func to include all cases and prevent brute force

--- api/CryptoClasses/refresh_token.py
+++ api/CryptoClasses/refresh_token.py
@@ -6,6 +6,7 @@ import datetime as dt
 from connexion.exceptions import Forbidden, Unauthorized
 from database.rate_limiting_repo import RateLimitingRepo
 from database.session_token_repo import SessionTokenRepo
+import Utils.utils as utils
 
 
 
@@ -19,15 +20,13 @@ def generate_refresh_token(user_id, session_token_id, expiration=-1):
     
 def refresh_token_flow(refresh, session, ip):
         rate_limiting = RateLimitingRepo()
-        rt_repo = RefreshTokenRepo()
         session_repo = SessionTokenRepo()
         if refresh.session_token_id == session.id and refresh.user_id == session.user_id:
             if refresh.revoke_timestamp == None:
                 if float(refresh.expiration) > dt.datetime.now(dt.UTC).timestamp():
                     new_session_id, new_session_token = session_repo.generate_session_token(session.user_id)
                     new_refresh_token = generate_refresh_token(session.user_id, new_session_id, expiration=refresh.expiration)
-                    rt_repo.revoke(refresh.id)
-                    session_repo.revoke(session.id)
+                    utils.revoke_session(session_id=session.id, refresh_id=refresh.id)
                     return new_session_token, new_refresh_token
                 else:
                     rate_limiting.add_failed_login(ip, refresh.user_id)
@@ -40,8 +39,7 @@ def refresh_token_flow(refresh, session, ip):
         else:
             rate_limiting.add_failed_login(ip, session.user_id)
             logging.warning(f"A refresh token has been asked, but invalid context for refresh token {refresh.id}. Session : Id : {session.id}, User {session.user_id}. Refresh : Id : {refresh.id}, User: {refresh.user_id}, Association session id : {refresh.session_token_id}. Refresh flow aborted.")
-            rt_repo.revoke(refresh.id)
-            session_repo.revoke(session.id)
+            utils.revoke_session(session_id=session.id, refresh_id=refresh.id)
             raise Forbidden("Access denied")
 
 
