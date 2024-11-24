d0143dad8122cc79137948aa3af43d4c8e7f4d2a
seaweedbrain <nathanstchepinsky@gmail.com>
2024-11-24 00:48:17 -0500

    Use session token instead of jwt

--- api/tests/unit/test_delete_account.py
+++ api/tests/unit/test_delete_account.py
@@ -4,16 +4,15 @@ from database.db import db
 from environment import conf
 from zero_totp_db_model.model import User as UserModel
 from unittest.mock import patch
-from CryptoClasses.jwt_func import generate_jwt, ISSUER as jwt_ISSUER, ALG as jwt_ALG
 import datetime
-import jwt
 from database.user_repo import User as User_repo
 from database.totp_secret_repo import TOTP_secret as TOTP_secret_repo
 from database.zke_repo import ZKE as ZKE_encryption_key_repo
 from database.google_drive_integration_repo import GoogleDriveIntegration as GoogleDriveIntegration_repo
 from database.preferences_repo import Preferences as Preferences_repo
 from database.oauth_tokens_repo import Oauth_tokens as Oauth_tokens_repo
-from zero_totp_db_model.model import Admin as AdminModel, Preferences as PreferencesModel
+from zero_totp_db_model.model  import Preferences as PreferencesModel
+from database.session_token_repo import SessionTokenRepo 
 from uuid import uuid4
 from environment import conf
 
@@ -40,6 +39,7 @@ class TestDeleteAccount(unittest.TestCase):
         self.google_drive_integration_repo = GoogleDriveIntegration_repo()
         self.preferences_repo = Preferences_repo()
         self.oauth_tokens_repo = Oauth_tokens_repo()
+        self.session_token_repo = SessionTokenRepo()
 
         self.delete_google_drive_option = patch("controllers.delete_google_drive_option").start()
         self.delete_google_drive_option.return_value = True
@@ -61,7 +61,7 @@ class TestDeleteAccount(unittest.TestCase):
             self.user_repo.create("user4", "user3@test.com", "password", "salt", "salt", "01/01/2001", isVerified=True, isBlocked=False)
             self.user_repo.create("user5", "user5@test.com", "password", "salt", "salt", "01/01/2001", isVerified=True, isBlocked=True)
             self.user_repo.create("user6", "user6@test.com", "password", "salt", "salt", "01/01/2001", isVerified=False, isBlocked=False)
-            self.user_repo.create("user7", "user7@test.com", "password", "salt", "salt", "01/01/2001", isVerified=False, isBlocked=False, role="admin")
+            self.user_repo.create("user7", "user7@test.com", "password", "salt", "salt", "01/01/2001", isVerified=True, isBlocked=False, role="admin")
 
             # User 1 :
             self.totp_secret_repo.add(self.full_user_id, "secret1", str(uuid4()))
@@ -80,6 +80,11 @@ class TestDeleteAccount(unittest.TestCase):
             self.totp_secret_repo.add(self.user_without_google_drive, "secret4", str(uuid4()))
             self.zke_encryption_key_repo.create(self.full_user_id, "zke1")
 
+            _, self.full_user_session_token = self.session_token_repo.generate_session_token(self.full_user_id)
+            _, self.user_without_google_drive_session_token = self.session_token_repo.generate_session_token(self.user_without_google_drive)
+            _, self.user_blocked_session_token = self.session_token_repo.generate_session_token(self.user_blocked)
+            _, self.user_not_verified_session_token = self.session_token_repo.generate_session_token(self.user_not_verified)
+            _, self.user_admin_session_token = self.session_token_repo.generate_session_token(self.user_admin_id)
             
             db.session.commit()
     
@@ -99,7 +104,7 @@ class TestDeleteAccount(unittest.TestCase):
 
     def test_delete_user_with_google_drive(self):
         with self.flask_application.app.app_context():
-            self.client.cookies= {"api-key" :generate_jwt(self.full_user_id)}
+            self.client.cookies= {"session-token" :self.full_user_session_token}
             self.client.headers = {"x-hash-passphrase": "passphrase"}
             response = self.client.delete(self.deleteEndpoint)
             self.assertEqual(response.status_code, 200)
@@ -114,7 +119,7 @@ class TestDeleteAccount(unittest.TestCase):
     
     def test_delete_user_without_google_drive(self):
         with self.flask_application.app.app_context():
-            self.client.cookies= {"api-key" :generate_jwt(self.user_without_google_drive)}
+            self.client.cookies= {"session-token" :self.user_without_google_drive_session_token}
             self.client.headers = {"x-hash-passphrase": "passphrase"}
             response = self.client.delete(self.deleteEndpoint)
             self.assertEqual(response.status_code, 200)
@@ -129,19 +134,18 @@ class TestDeleteAccount(unittest.TestCase):
     
     def test_delete_admin(self):
          with self.flask_application.app.app_context():
-            self.client.cookies= {"api-key" :generate_jwt(self.user_admin_id)}
+            self.client.cookies= {"session-token" :self.user_admin_session_token}
             self.client.headers = {"x-hash-passphrase": "passphrase"}
             response = self.client.delete(self.deleteEndpoint)
-            self.assertEqual(response.status_code, 403)
+            self.assertEqual(response.status_code, 403, f'Expected 403, got {response.status_code}. Response: {response.json()}')
+            self.assertEqual(response.json(),  {"message": "Admin cannot be deleted"})
             self.delete_google_drive_backup.assert_not_called()
             self.delete_google_drive_option.assert_not_called()
             self.assertNotEqual(self.user_repo.getById(self.user_admin_id), None)
-            self.assertNotEqual(self.totp_secret_repo.get_all_enc_secret_by_user_id(self.user_admin_id), [])
-            self.assertNotEqual(self.zke_encryption_key_repo.getByUserId(self.user_admin_id), None)
-
+            
     def test_delete_bad_passphrase(self):
         with self.flask_application.app.app_context():
-            self.client.cookies= {"api-key" :generate_jwt(self.full_user_id)}
+            self.client.cookies= {"session-token" :self.full_user_session_token}
             self.client.headers = {"x-hash-passphrase": "badPassphrase"}
             self.checkpw.return_value = False
             response = self.client.delete(self.deleteEndpoint)
@@ -150,7 +154,7 @@ class TestDeleteAccount(unittest.TestCase):
     
     def test_delete_no_passphrase_header(self):
         with self.flask_application.app.app_context():
-            self.client.cookies= {"api-key" :generate_jwt(self.full_user_id)}
+            self.client.cookies= {"session-token" :self.full_user_session_token}
             response = self.client.delete(self.deleteEndpoint)
             self.assertEqual(response.status_code, 401)
             self.assertNotEqual(self.user_repo.getById(self.full_user_id), None)
@@ -163,15 +167,15 @@ class TestDeleteAccount(unittest.TestCase):
     
     def test_bad_cookie(self):
         with self.flask_application.app.app_context():
-            self.client.cookies = {"api-key":"badCookie"}
+            self.client.cookies = {"session-token":"badCookie"}
             self.client.headers = {"x-hash-passphrase": "badPassphrase"}
             response = self.client.delete(self.deleteEndpoint)
-            self.assertEqual(response.status_code, 403)
+            self.assertEqual(response.status_code, 401)
             self.assertNotEqual(self.user_repo.getById(self.full_user_id), None)
 
     def test_delete_user_doesnt_exists(self):
         with self.flask_application.app.app_context():
-            self.client.cookies= {"api-key" :generate_jwt(10)}
+            self.client.cookies= {"session-token" :str(uuid4())}
             self.client.headers = {"x-hash-passphrase": "badPassphrase"}
             response = self.client.delete(self.deleteEndpoint)
             self.assertEqual(response.status_code, 401)
@@ -179,7 +183,7 @@ class TestDeleteAccount(unittest.TestCase):
     
     def test_delete_user_error_with_google_drive(self):
         with self.flask_application.app.app_context():
-            self.client.cookies= {"api-key" :generate_jwt(self.full_user_id)}
+            self.client.cookies= {"session-token" :self.full_user_session_token}
             self.client.headers = {"x-hash-passphrase": "passphrase"}
             self.delete_google_drive_backup.side_effect = Exception("error")
             response = self.client.delete(self.deleteEndpoint)
@@ -194,7 +198,7 @@ class TestDeleteAccount(unittest.TestCase):
 
     def test_delete_user_error_while_deleting_from_db(self):
         with self.flask_application.app.app_context():
-            self.client.cookies= {"api-key" :generate_jwt(self.full_user_id)}
+            self.client.cookies= {"session-token" :self.full_user_session_token}
             self.client.headers = {"x-hash-passphrase": "passphrase"}
             self.secrets_delete_all = patch("database.totp_secret_repo.TOTP_secret.delete_all").start()
             self.secrets_delete_all.side_effect = Exception("error")
@@ -203,7 +207,7 @@ class TestDeleteAccount(unittest.TestCase):
     
     def test_delete_user_not_verified(self):
         with self.flask_application.app.app_context():
-            self.client.cookies= {"api-key" :generate_jwt(self.user_not_verified)}
+            self.client.cookies= {"session-token" :self.user_not_verified_session_token}
             self.client.headers = {"x-hash-passphrase": "passphrase"}
             response = self.client.delete(self.deleteEndpoint)
             self.assertEqual(response.status_code, 403)
@@ -211,7 +215,7 @@ class TestDeleteAccount(unittest.TestCase):
     
     def test_delete_user_blocked(self):
         with self.flask_application.app.app_context():
-            self.client.cookies= {"api-key" :generate_jwt(self.user_blocked)}
+            self.client.cookies= {"session-token" :self.user_blocked_session_token}
             self.client.headers = {"x-hash-passphrase": "passphrase"}
             response = self.client.delete(self.deleteEndpoint)
             self.assertEqual(response.status_code, 403)
