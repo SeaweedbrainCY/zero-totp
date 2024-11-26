80ba068d1d1a2aac7015211736868fa98f501f66
seaweedbrain <nathanstchepinsky@gmail.com>
2024-11-25 19:22:05 -0500

    Return 403 if token verification fail

--- api/CryptoClasses/session_verification.py
+++ api/CryptoClasses/session_verification.py
@@ -14,9 +14,9 @@ def verify_session(token_info):
         remote_ip = get_ip(request)
         session_token = SessionTokenRepo().get_session_token(token_info)
         if not session_token:
-            raise Unauthorized("Invalid session token")
+            raise Forbidden("Invalid session token")
         if float(session_token.expiration) < dt.datetime.now(dt.UTC).timestamp():
-            raise Unauthorized("API key expired")
+            raise Forbidden("API key expired")
         if session_token.revoke_timestamp is not None:
             logging.info(f"Rejected session token {session_token.id} because it was revoked. User {session_token.user_id} from IP {remote_ip}")
             raise Forbidden("Invalid session token")
