594c1080788f763e257a48c4aa9447e0c0a74ef2
seaweedbrain <nathanstchepinsky@gmail.com>
2024-11-30 19:25:07 -0500

    Use session token instead of jwt

--- api/tests/unit/test_update_vault_controller.py
+++ api/tests/unit/test_update_vault_controller.py
@@ -4,12 +4,12 @@ import unittest
 import controllers
 from unittest.mock import patch
 from zero_totp_db_model.model import User, TOTP_secret, ZKE_encryption_key
+from database.session_token_repo  import SessionTokenRepo
 from environment import conf
-from CryptoClasses import jwt_func,hash_func
 from uuid import uuid4
 import json
-import jwt
 import datetime
+from CryptoClasses import hash_func
 
 class TestUpdateVault(unittest.TestCase):
 
@@ -25,10 +25,12 @@ class TestUpdateVault(unittest.TestCase):
         self.foreign_totp_id = str(uuid4())
         self.nb_totp = 10
         self.password =  hash_func.Bcrypt("HelloWorld!")
-        self.jwtCookie = jwt_func.generate_jwt(self.user_id)
         user = User(id=self.user_id,username='user1', mail="user1@test.com", password=self.password.hashpw().decode('utf-8'), derivedKeySalt="AAA", isVerified = True, passphraseSalt = "AAA", createdAt="01/01/2001", isBlocked=False)
         self.totp_codes = {}
         zke = ZKE_encryption_key(user_id=self.user_id, ZKE_key="zke_enc")
+
+        self.session_token_repo = SessionTokenRepo()
+
         with self.flask_application.app.app_context():
             db.create_all()
             db.session.add(user)
@@ -41,6 +43,7 @@ class TestUpdateVault(unittest.TestCase):
             self.foreign_totp = TOTP_secret(uuid=self.foreign_totp_id, user_id=self.other_user_id, secret_enc = "enc")
             db.session.add(self.foreign_totp)
             db.session.commit()
+            _, self.session_token = self.session_token_repo.generate_session_token(self.user_id)
 
             
        
@@ -62,20 +65,11 @@ class TestUpdateVault(unittest.TestCase):
             db.drop_all()
             patch.stopall()
     
-    def generate_expired_cookie(self):
-        payload = {
-            "iss": jwt_func.ISSUER,
-            "sub": 1,
-            "iat": datetime.datetime.utcnow(),
-            "nbf": datetime.datetime.utcnow(),
-            "exp": datetime.datetime.utcnow() - datetime.timedelta(hours=1),
-        }
-        jwtCookie = jwt.encode(payload, conf.api.jwt_secret, algorithm=jwt_func.ALG)
-        return jwtCookie
+    
     
     def test_update_vault(self):
         with self.flask_application.app.app_context():
-            self.client.cookies = {"api-key": self.jwtCookie}
+            self.client.cookies = {"session-token": self.session_token}
             response = self.client.put(self.endpoint, json=self.payload)
             self.assertEqual(response.status_code, 201)
             codes = db.session.query(TOTP_secret).filter_by(user_id=self.user_id).all()
@@ -88,24 +82,10 @@ class TestUpdateVault(unittest.TestCase):
             zke = db.session.query(ZKE_encryption_key).filter_by(user_id=self.user_id).first()
             self.assertEqual(zke.ZKE_key, self.payload["zke_enc"])
 
-    
 
-    def test_update_vault_no_cookie(self):
-        response = self.client.put(self.endpoint)
-        self.assertEqual(response.status_code, 401)
-    
-    def test_update_vault_bad_cookie(self):
-        self.client.cookies = {"api-key": "badcookie"}
-        response = self.client.put(self.endpoint)
-        self.assertEqual(response.status_code, 403)
-    
-    def test_update_vault_expired_jwt(self):
-        self.client.cookies = {"api-key": self.generate_expired_cookie()}
-        response = self.client.put(self.endpoint)
-        self.assertEqual(response.status_code, 401)
 
     def test_update_vault_bad_args(self):
-        self.client.cookies = {"api-key": self.jwtCookie}
+        self.client.cookies = {"session-token": self.session_token}
         for key in self.payload.keys():
             payload = self.payload.copy()
             payload.pop(key)
@@ -121,7 +101,7 @@ class TestUpdateVault(unittest.TestCase):
 
     def test_update_vault_wrong_passphrase(self):
         self.payload["old_passphrase"] = "wrong"
-        self.client.cookies = {"api-key": self.jwtCookie}
+        self.client.cookies = {"session-token": self.session_token}
         response = self.client.put(self.endpoint, json=self.payload)
         self.assertEqual(response.status_code, 403)
         self.assertEqual(response.json(), {"message": "Invalid passphrase"})
@@ -129,7 +109,7 @@ class TestUpdateVault(unittest.TestCase):
 
     def test_update_vault_passphrase_too_long(self):
         self.payload["new_passphrase"] = "a"*100
-        self.client.cookies = {"api-key": self.jwtCookie}
+        self.client.cookies = {"session-token": self.session_token}
         response = self.client.put(self.endpoint, json=self.payload)
         self.assertEqual(response.status_code, 400)
         self.assertEqual({"message": "passphrase too long. It must be <70 char"}, response.json())
@@ -139,7 +119,7 @@ class TestUpdateVault(unittest.TestCase):
         self.totp_codes.pop(list(self.totp_codes.keys())[0])
         self.totp_codes[str(uuid4())] = "code"
         self.payload["enc_vault"] = json.dumps(self.totp_codes)
-        self.client.cookies = {"api-key": self.jwtCookie}
+        self.client.cookies = {"session-token": self.session_token}
         response = self.client.put(self.endpoint, json=self.payload)
         self.assertEqual(response.status_code, 403)
         self.assertEqual(response.json(), {"message": "Forbidden action. Zero-TOTP detected that you were updating object you don't have access to. The request is rejected."}, 403)
@@ -148,7 +128,7 @@ class TestUpdateVault(unittest.TestCase):
         self.totp_codes.pop(list(self.totp_codes.keys())[0])
         self.totp_codes[self.foreign_totp_id] = "code"
         self.payload["enc_vault"] = json.dumps(self.totp_codes)
-        self.client.cookies = {"api-key": self.jwtCookie}
+        self.client.cookies = {"session-token": self.session_token}
         response = self.client.put(self.endpoint, json=self.payload)
         self.assertEqual(response.status_code, 403)
         self.assertEqual(response.json(), {"message": "Forbidden action. Zero-TOTP detected that you were updating object you don't have access to. The request is rejected."}, 403)
@@ -156,7 +136,7 @@ class TestUpdateVault(unittest.TestCase):
     def test_update_vault_too_many_totp(self):
         self.totp_codes[str(uuid4())] = "code"
         self.payload["enc_vault"] = json.dumps(self.totp_codes)
-        self.client.cookies = {"api-key": self.jwtCookie}
+        self.client.cookies = {"session-token": self.session_token}
         response = self.client.put(self.endpoint, json=self.payload)
         self.assertEqual(response.status_code, 400)
         self.assertEqual(response.json(), {"message": "To avoid the loss of your information, Zero-TOTP is rejecting this request because it has detected that you might lose data. Please contact quickly Zero-TOTP developers to fix issue."}, 400)
@@ -164,7 +144,7 @@ class TestUpdateVault(unittest.TestCase):
     def test_update_vault_too_few_totp(self):
         self.totp_codes.pop(list(self.totp_codes.keys())[0])
         self.payload["enc_vault"] = json.dumps(self.totp_codes)
-        self.client.cookies = {"api-key": self.jwtCookie}
+        self.client.cookies = {"session-token": self.session_token}
         response = self.client.put(self.endpoint, json=self.payload)
         self.assertEqual(response.status_code, 400)
         self.assertEqual(response.json(), {"message": "To avoid the loss of your information, Zero-TOTP is rejecting this request because it has detected that you might lose data. Please contact quickly Zero-TOTP developers to fix issue."}, 400)
@@ -176,7 +156,7 @@ class TestUpdateVault(unittest.TestCase):
             user = db.session.query(User).filter_by(id=self.user_id).first()
             user.isVerified = False
             db.session.commit()
-            self.client.cookies = {"api-key": self.jwtCookie}
+            self.client.cookies = {"session-token": self.session_token}
             response = self.client.put(self.endpoint, json=self.payload)
             self.assertEqual(response.status_code, 403)
             self.assertEqual(response.json()["error"], "Not verified")
@@ -189,7 +169,7 @@ class TestUpdateVault(unittest.TestCase):
             user = db.session.query(User).filter_by(id=self.user_id).first()
             user.isBlocked = True
             db.session.commit()
-            self.client.cookies = {"api-key": self.jwtCookie}
+            self.client.cookies = {"session-token": self.session_token}
             response = self.client.put(self.endpoint, json=self.payload)
             self.assertEqual(response.status_code, 403)
             self.assertEqual(response.json()["error"], "User is blocked")
@@ -198,7 +178,7 @@ class TestUpdateVault(unittest.TestCase):
             db.session.commit()
     
     def test_update_invalid_vault(self):
-        self.client.cookies = {"api-key": self.jwtCookie}
+        self.client.cookies = {"session-token": self.session_token}
         payload = self.payload.copy()
         payload["enc_vault"] = '{"test": "secret"}'
         response = self.client.put(self.endpoint, json=payload)
