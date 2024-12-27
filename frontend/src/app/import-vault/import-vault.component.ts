a0d49e46b25068d60b3d52a9e1761f61a289711e
seaweedbrain <nathanstchepinsky@gmail.com>
2024-12-26 20:39:33 -0500

    Remove merging selection and add partial support of vault decryption Huge improvement of the UI and use the ne vault service UX massively improved with a new routing system and a clear handling of errors

--- frontend/src/app/import-vault/import-vault.component.ts
+++ frontend/src/app/import-vault/import-vault.component.ts
@@ -2,12 +2,13 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { faFileArrowDown, faArrowRight, faCloudArrowUp, faCheck, faUnlockKeyhole, faLock, faUnlock, faCircleCheck, faCircleNotch } from '@fortawesome/free-solid-svg-icons';
 import { faCircle } from '@fortawesome/free-regular-svg-icons';
 import { TranslateService } from '@ngx-translate/core';
-import { Router, ActivatedRoute } from '@angular/router';
+import { Router, ActivatedRoute, RouterStateSnapshot, NavigationEnd } from '@angular/router';
 import { ViewportRuler } from '@angular/cdk/scrolling';
 import { NgZone } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
 import { LocalVaultV1Service, UploadVaultStatus } from '../common/upload-vault/LocalVaultv1Service.service';
 import { Utils } from '../common/Utils/utils';
+import { VaultService } from '../common/VaultService/vault.service';
 
 
 
@@ -30,10 +31,10 @@ export class ImportVaultComponent implements OnInit, OnDestroy {
   faCircleEmpty = faCircle;
   faCircleCheck = faCircleCheck;
   faCircleNotch = faCircleNotch;
-  vault_password = "";
   continue_button_text = "continue";
   local_vault_service: LocalVaultV1Service | null = null;
   isUnsecureVaultModaleActive = false;
+  imported_vault_passphrase = "";
 
   isMobileDevice = false;
 
@@ -45,6 +46,10 @@ export class ImportVaultComponent implements OnInit, OnDestroy {
 
   width: number = 0;
   height: number = 0;
+
+  decrypted_vault: Map<string, Map<string, string>> | undefined;
+  decryption_error: string | undefined;
+
 
   private readonly viewportChange = this.viewportRuler
     .change(200)
@@ -62,9 +67,10 @@ export class ImportVaultComponent implements OnInit, OnDestroy {
     private readonly ngZone: NgZone,
     private toastr: ToastrService,
     private localVaultv1: LocalVaultV1Service,
-    private utils: Utils
+    private utils: Utils,
+    private vaultService: VaultService
   ) {
-    this.vault_steps.set("zero-totp", ["import", "decrypt", "select", "encrypt"])
+    this.vault_steps.set("zero-totp", ["import", "decrypt", "encrypt"])
     this.setSize();
 
   }
@@ -73,12 +79,9 @@ export class ImportVaultComponent implements OnInit, OnDestroy {
 
     this.init_component()
 
-    this.router.events.subscribe((url: any) => {
+    this.route.paramMap.subscribe(params => {
       this.init_component()
-    });
-
-
-
+    })
 
 
   }
@@ -94,6 +97,7 @@ export class ImportVaultComponent implements OnInit, OnDestroy {
     console.log(this.step)
     if (this.vault_type != null) {
       if (!(this.vault_steps.has(this.vault_type))) {
+        console.log("not found. Redirecting to import")
         this.router.navigate(['/import/vault'])
       }
       if (this.vault_type == "zero-totp") {
@@ -104,9 +108,19 @@ export class ImportVaultComponent implements OnInit, OnDestroy {
             this.redirectToFirstStep()
           } else {
             if (this.step == "import") {
-              this.is_continue_disabled = true;
+              if(this.local_vault_service == null){
+                this.is_continue_disabled = true;
+              } else {
+                this.is_continue_disabled = false;
+              }
             } else if (this.step == "decrypt") {
-              this.is_continue_disabled = true;
+              if(this.local_vault_service == null){
+                console.log("null")
+                this.router.navigate(['/import/vault/zero-totp/import'])
+              } else {
+                this.is_continue_disabled = true;
+              }
+              
             } else if (this.step == "select") {
               this.is_continue_disabled = true;
             } else if (this.step == "encrypt") {
@@ -158,6 +172,8 @@ export class ImportVaultComponent implements OnInit, OnDestroy {
                   switch (vault_parsing_status) {
                     case UploadVaultStatus.SUCCESS: {
                       this.file_name = input.files[0].name
+                      this.is_continue_disabled = false;
+                      
 
                       break
                     }
@@ -165,6 +181,8 @@ export class ImportVaultComponent implements OnInit, OnDestroy {
                       this.translate.get("login.errors.import_vault.invalid_type").subscribe((translation) => {
                         this.utils.toastError(this.toastr, translation, "");
                       });
+                      this.local_vault_service = null;
+                      event.target.value = null;
 
                       break;
                     }
@@ -173,25 +191,31 @@ export class ImportVaultComponent implements OnInit, OnDestroy {
                       this.translate.get("login.errors.import_vault.invalid_version").subscribe((translation) => {
                         this.utils.toastError(this.toastr, translation, "");
                       });
-
+                      this.local_vault_service = null;
+                      event.target.value = null;
                       break;
                     }
                     case UploadVaultStatus.NO_SIGNATURE: {
                       this.translate.get("login.errors.import_vault.no_signature").subscribe((translation) => {
                         this.utils.toastError(this.toastr, translation, "")
                       });
+                      this.local_vault_service = null;
+                      event.target.value = null;
 
                       break;
                     }
                     case UploadVaultStatus.INVALID_SIGNATURE: {
                       this.isUnsecureVaultModaleActive = true;
                       this.file_name = input.files[0].name
+                      
                       break;
                     }
                     case UploadVaultStatus.MISSING_ARGUMENT: {
                       this.translate.get("login.errors.import_vault.missing_arg").subscribe((translation) => {
                         this.utils.toastError(this.toastr, translation, "")
                       });
+                      this.local_vault_service = null;
+                      event.target.value = null;
 
                       break;
                     }
@@ -199,6 +223,8 @@ export class ImportVaultComponent implements OnInit, OnDestroy {
                       this.translate.get("login.errors.import_vault.invalid_arg").subscribe((translation) => {
                         this.utils.toastError(this.toastr, translation, "")
                       });
+                      this.local_vault_service = null;
+                      event.target.value = null;
 
                       break;
                     }
@@ -207,6 +233,8 @@ export class ImportVaultComponent implements OnInit, OnDestroy {
                       this.translate.get("login.errors.import_vault.error_unknown").subscribe((translation) => {
                         this.utils.toastError(this.toastr, translation, "")
                       });
+                      this.local_vault_service = null;
+                      event.target.value = null;
 
                       break;
                     }
@@ -215,6 +243,8 @@ export class ImportVaultComponent implements OnInit, OnDestroy {
                       this.translate.get("login.errors.import_vault.error_unknown").subscribe((translation) => {
                         this.utils.toastError(this.toastr, translation, "")
                       });
+                      this.local_vault_service = null;
+                      event.target.value = null;
 
                       break;
                     }
@@ -225,23 +255,32 @@ export class ImportVaultComponent implements OnInit, OnDestroy {
                 this.translate.get("login.errors.import_vault.invalid_version").subscribe((translation) => {
                   this.utils.toastError(this.toastr, translation, "")
                 });
+                this.local_vault_service = null;
+                event.target.value = null;
 
               }
             } catch (e) {
+              console.log("Zero-totp vault parsing error: " + e)
               this.translate.get("login.errors.import_vault.parse_fail").subscribe((translation) => {
                 this.utils.toastError(this.toastr, translation, "")
               });
+              this.local_vault_service = null;
+              event.target.value = null;
 
             }
           } else {
             this.translate.get("login.errors.import_vault.parse_fail").subscribe((translation) => {
               this.utils.toastError(this.toastr, translation, "")
             });
+            this.local_vault_service = null;
+            event.target.value = null;
 
           }
         });
       } catch {
         this.is_importing = false
+        this.local_vault_service = null;
+        event.target.value = null;
       }
 
     }
@@ -287,6 +326,51 @@ export class ImportVaultComponent implements OnInit, OnDestroy {
     acceptUnsecureVault(){
       this.local_vault_service!.set_is_signature_valid(true); 
       this.isUnsecureVaultModaleActive = false;
+      this.is_continue_disabled = false;
+    }
+
+    rejectUnsecureVault(){
+      this.local_vault_service = null;
+      this.isUnsecureVaultModaleActive = false;
+      this.is_continue_disabled = true;
+    }
+
+
+    decrypt(){
+      this.decryption_error = "";
+      if(this.local_vault_service != null){
+        this.vaultService.derivePassphrase(this.local_vault_service!.get_derived_key_salt()!, this.imported_vault_passphrase).then((derivedKey)=>{
+          this.vaultService.decryptZKEKey(this.local_vault_service!.get_zke_key_enc()!, derivedKey, true).then((zke_key)=>{
+            this.vaultService.decryptVault(this.local_vault_service!.get_enc_secrets()!, zke_key).then((decrypted_vault)=>{
+              this.decrypted_vault = decrypted_vault;
+
+            }, (error)=>{
+              this.translate.get("import_vault.errors.decryption_failure").subscribe((translation)=>{
+               this.decryption_error = translation + ". Error: " + error;
+              });
+
+            });
+
+          }, (error)=>{
+            this.translate.get("import_vault.errors.bad_passphrase").subscribe((translation)=>{
+             this.decryption_error = translation;
+            });
+          });
+        }, 
+        (error)=>{
+          this.translate.get("import_vault.errors.derivation_failure").subscribe((translation)=>{
+            this.decryption_error = translation;
+          });
+        });;
+
+
+      } else {
+        this.translate.get("login.errors.import_vault.invalid_file").subscribe((translation) => {
+          this.utils.toastError(this.toastr, translation, "");
+        });
+        const current_step_index = this.vault_steps.get(this.vault_type!)!.indexOf(this.step!)
+        this.router.navigate(['/import/vault/' + this.vault_type + '/' + this.vault_steps.get(this.vault_type!)![current_step_index - 1]])
+      }
     }
 
 
