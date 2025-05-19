fde301eb8b7932bc8fa0e0532aef27cf48595087
SeaweedbrainCY <nathanstchepinsky@gmail.com>
2025-05-18 21:36:13 -0400

    Encode verifying token with hex instead of base64 to improve readiness

--- api/Utils/utils.py
+++ api/Utils/utils.py
@@ -90,7 +90,7 @@ def delete_user_from_database(user_id):
 def generate_new_email_verification_token(user_id):
     email_verification_token_repo = EmailVerificationToken()
     email_verification_token_repo.delete(user_id)
-    token = b64encode(os.urandom(5)).decode()
+    token = os.urandom(5).hex()
     expiration = datetime.datetime.utcnow() + datetime.timedelta(minutes=30)
     email_verification_token_repo.add(user_id, token,expiration.timestamp())
     return token
