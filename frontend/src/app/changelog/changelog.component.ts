6b5be923a1a9e73423be6122df35718941ac5785
SeaweedbrainCY <nathanstchepinsky@gmail.com>
2025-06-04 23:02:52 -0400

    add changelog of v1.4.0

--- frontend/src/app/changelog/changelog.component.ts
+++ frontend/src/app/changelog/changelog.component.ts
@@ -15,6 +15,13 @@ export class ChangelogComponent {
 
   changelogs = [
     {
+      date: "04/06/2025",
+      version: "1.4.0",
+      fixed:[
+        "A lot of packages have been updated on the frontend and API side to improve the stability and security of the application.",
+      ]
+    },
+    {
       date: "25/05/2025",
       version: "1.3.0",
       added: [
