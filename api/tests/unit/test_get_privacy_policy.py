24767ea5ab3ad08d649c48643d5550eaf99a2156
SeaweedbrainCY <nathanstchepinsky@gmail.com>
2025-06-07 16:46:03 -0400

    Update tests to verify the returned privacy policy is correct

--- api/tests/unit/test_get_privacy_policy.py
+++ api/tests/unit/test_get_privacy_policy.py
@@ -16,28 +16,27 @@ class TestGetPrivacyPolicy(unittest.TestCase):
     def test_get_english_privacy_policy(self):
         response = self.client.get(f"{self.endpoint}?lang=en")
         self.assertEqual(response.status_code, 200)
-        data = response.get_json()
-        self.assertIn("markdown", data)
+        self.assertEqual(response.content_type, "text/markdown; charset=utf-8")
+        data = response.get_data(as_text=True)
         with open(conf.features.privacy_policy.privacy_policy_mk_file_path["en"], "r") as f:
             expected_content = f.read()
-        self.assertEqual(data["markdown"], expected_content)   
+        self.assertEqual(data, expected_content)
 
     def test_get_french_privacy_policy(self):
         response = self.client.get(f"{self.endpoint}?lang=fr")
         self.assertEqual(response.status_code, 200)
-        data = response.get_json()
-        self.assertIn("markdown", data)
+        self.assertEqual(response.content_type, "text/markdown; charset=utf-8")
+        data = response.get_data(as_text=True)
         with open(conf.features.privacy_policy.privacy_policy_mk_file_path["fr"], "r") as f:
             expected_content = f.read()
-        self.assertEqual(data["markdown"], expected_content)
+        self.assertEqual(data, expected_content)
 
     def test_get_invalid_language_privacy_policy(self):
-        response = self.client.get(f"{self.endpoint}?lang=invalid")
+        response = self.client.get(f"{self.endpoint}?lang=xx")
         self.assertEqual(response.status_code, 200)
-        data = response.get_json()
-        self.assertIn("markdown", data)
+        self.assertEqual(response.content_type, "text/markdown; charset=utf-8")
+        data = response.get_data(as_text=True)
         with open(conf.features.privacy_policy.privacy_policy_mk_file_path["en"], "r") as f:
             expected_content = f.read()
-        self.assertEqual(data["markdown"], expected_content)
-    
+        self.assertEqual(data, expected_content)
     
