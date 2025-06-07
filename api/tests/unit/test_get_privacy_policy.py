47bc4f4df1412f121e2a251a18052cf4c3b081c4
SeaweedbrainCY <nathanstchepinsky@gmail.com>
2025-06-07 17:04:55 -0400

    Fix tests using starlette spec

--- api/tests/unit/test_get_privacy_policy.py
+++ api/tests/unit/test_get_privacy_policy.py
@@ -1,6 +1,6 @@
 import unittest
 from app import app
-from environment import conf
+from environment import conf, logging
 
 
 class TestGetPrivacyPolicy(unittest.TestCase):
@@ -16,8 +16,8 @@ class TestGetPrivacyPolicy(unittest.TestCase):
     def test_get_english_privacy_policy(self):
         response = self.client.get(f"{self.endpoint}?lang=en")
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.content_type, "text/markdown; charset=utf-8")
-        data = response.get_data(as_text=True)
+        self.assertEqual(response.headers.get("content-type"), "text/markdown; charset=utf-8")
+        data = response.content.decode('utf-8')
         with open(conf.features.privacy_policy.privacy_policy_mk_file_path["en"], "r") as f:
             expected_content = f.read()
         self.assertEqual(data, expected_content)
@@ -25,17 +25,18 @@ class TestGetPrivacyPolicy(unittest.TestCase):
     def test_get_french_privacy_policy(self):
         response = self.client.get(f"{self.endpoint}?lang=fr")
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.content_type, "text/markdown; charset=utf-8")
-        data = response.get_data(as_text=True)
+        self.assertEqual(response.headers.get("content-type"), "text/markdown; charset=utf-8")
+        data = response.content.decode('utf-8')
         with open(conf.features.privacy_policy.privacy_policy_mk_file_path["fr"], "r") as f:
             expected_content = f.read()
         self.assertEqual(data, expected_content)
 
     def test_get_invalid_language_privacy_policy(self):
         response = self.client.get(f"{self.endpoint}?lang=xx")
+
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.content_type, "text/markdown; charset=utf-8")
-        data = response.get_data(as_text=True)
+        self.assertEqual(response.headers.get("content-type"), "text/markdown; charset=utf-8")
+        data = response.content.decode('utf-8')
         with open(conf.features.privacy_policy.privacy_policy_mk_file_path["en"], "r") as f:
             expected_content = f.read()
         self.assertEqual(data, expected_content)
