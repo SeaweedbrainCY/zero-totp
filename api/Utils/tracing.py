cbbefca127face75d345eac4f55dedf56541c6f5
SeaweedbrainCY <nathanstchepinsky@gmail.com>
2025-06-14 01:02:21 -0400

    Add opentelemetry tracing

---
+++ api/Utils/tracing.py
@@ -0,0 +1,32 @@
+from opentelemetry import trace
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.resources import SERVICE_NAME, Resource
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
+from environment import conf, logging
+
+# Service name is required for most backends
+resource = Resource(attributes={
+    SERVICE_NAME: "python-service"
+})
+
+# create a tracer provider
+tracer_provider = TracerProvider(resource=resource)
+
+# create an OTLP trace exporter
+if conf.features.tracing.openobserve is not None:
+    url = conf.features.tracing.openobserve.url
+    headers = {"Authorization": f"Basic conf.features.tracing.openobserve.access_token"}
+    logging.info(f"OpenObserve tracing enabled.")
+
+
+exporter = OTLPSpanExporter(endpoint=url, headers=headers)
+
+# create a span processor to send spans to the exporter
+span_processor = BatchSpanProcessor(exporter)
+
+# add the span processor to the tracer provider
+tracer_provider.add_span_processor(span_processor)
+
+# set the tracer provider as the global provider
+trace.set_tracer_provider(tracer_provider)
