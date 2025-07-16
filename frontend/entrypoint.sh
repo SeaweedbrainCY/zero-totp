0f1f339cb5c995011e6aeb87d638ed28a0c47e66
seaweedbraincy <nathanstchepinsky@gmail.com>
2025-07-16 21:50:42 +0000

    Create logs file and actively check if frontend is healthy

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -13,6 +13,8 @@ fi
 
 touch /var/run/nginx.pid
 mkdir -p /var/cache/nginx /var/log/nginx
+touch /var/log/nginx/error.log
+touch /var/log/nginx/access.log
 chown -R $USER_UID:$USER_GID /var/cache/nginx /var/run/nginx.pid /var/log/nginx
 echo "Starting nginx"
 echo "Logs will be written to /var/log/nginx/error.log and /var/log/nginx/access.log"
