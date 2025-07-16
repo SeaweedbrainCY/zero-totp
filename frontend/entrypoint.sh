668600f3e13762c5dca44c27a03cc0d160f7ef20
SeaweedbrainCY <nathanstchepinsky@gmail.com>
2025-07-16 00:29:32 -0400

    Add entrypoint to manage root privileges drop and configuration

---
+++ frontend/entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+set -e
+
+if [ "$USER_UID" = "" ]; then
+    echo "USER_UID environment variable is not set. Defaulting to 101 (nginx)"
+    USER_UID="101"
+fi
+
+if [ "$USER_GID" = "" ]; then
+    echo "USER_GID environment variable is not set. Defaulting to 101 (nginx)"
+    USER_GID="101"
+fi
+
+touch /var/run/nginx.pid
+chown -R $USER_UID:$USER_GID /var/cache/nginx /var/run/nginx.pid /var/log/nginx
+
+exec su-exec "$NGINX_USER" nginx -g "daemon off;"
+
