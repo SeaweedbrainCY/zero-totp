dfa62ca8d4d23fc8de8b809ddfca20360775ba68
SeaweedbrainCY <nathanstchepinsky@gmail.com>
2026-04-10 10:45:47 +0200

    refactor(frontend): Switch user services as signal

--- frontend/src/app/services/User/user.service.ts
+++ frontend/src/app/services/User/user.service.ts
@@ -1,26 +1,24 @@
-import { Injectable } from '@angular/core';
-import { Utils }  from '../Utils/utils';
+import { Injectable, WritableSignal, signal } from '@angular/core';
 import { LocalVaultV1Service } from '../upload-vault/LocalVaultv1Service.service';
 import { HttpClient } from '@angular/common/http';
-@Injectable({providedIn: 'root'}) //Note that you will need to declare it as `@Injectable`. 
+@Injectable({providedIn: 'root'})
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-  private id:number| null =null;;
-  private email:string| null =null;;
-  private zke_key:CryptoKey| null =null;;
-  private derivedKeySalt: string| null =null;;
-  private vault:Map<string, Map<string,string>> | null =null;
-  private passphraseSalt: string| null =null;;
-  private isVaultLocal:boolean = false;
-  private local_vault_service:LocalVaultV1Service | null = null;
-  private googleDriveSync:boolean | null = null;
-  private vault_tags:string[] = [];
+  public id:  WritableSignal<number| null> = signal(null);
+  public email: WritableSignal<string| null> = signal(null);
+  public zke_key: WritableSignal<CryptoKey| null> = signal(null);
+  public derivedKeySalt: WritableSignal<string| null> = signal(null);
+  public vault: WritableSignal<Map<string, Map<string,string>> | null> = signal(null);
+  public passphraseSalt: WritableSignal<string| null> = signal(null);
+  public isVaultLocal: WritableSignal<boolean> = signal(false);
+  public local_vault_service: WritableSignal<LocalVaultV1Service | null> = signal(null);
+  public googleDriveSync: WritableSignal<boolean | null> = signal(null);
+  public vault_tags: WritableSignal<string[]> = signal([]);
 
   constructor(
-    private utils: Utils,
     private http: HttpClient
   ) {
    }
@@ -31,8 +29,8 @@ export class UserService {
         next: (response) => {
           if (response.status === 200) {
             const userData = response.body as { id: number, email: string, username: string };
-            this.setId(userData.id);
-            this.setEmail(userData.email);
+            this.id.set(userData.id);
+            this.email.set(userData.email);
             localStorage.setItem("email", userData.email);
             resolve(true);
           } else {
@@ -48,109 +46,31 @@ export class UserService {
     });
    }
 
-   getId() : number | null {
-    return this.id;
+   // Check if the vault is loaded and can be decrypted. If not, the user will have to import a local one or provide their passphrase to decrypt it
+   isVaultLoadedAndDecryptable() : boolean {
+    return this.isVaultLocal() && this.zke_key != null
    }
-
-   setId(id:number){
-    this.id = id;
-   }
-
-   getEmail() : string | null{
-    return this.utils.sanitize(this.email);
-   }
-
-   setEmail(email:string){
-    this.email = this.utils.sanitize(email);
-   }
-
-   get_zke_key() : CryptoKey | null{
-    return this.zke_key;
-   }
-
-   set_zke_key(zke_key:CryptoKey){
-    this.zke_key = zke_key;
-   }
-
-   getDerivedKeySalt(): string | null {
-    return this.derivedKeySalt;
-   }
-
-   setDerivedKeySalt(salt:string){
-    this.derivedKeySalt = salt;
-   }
-
-   getVault(): Map<string, Map<string,string>>| null {
-    return this.vault;
-   }
-
-   setVault(vault:Map<string, Map<string,string>>){
-    this.vault = vault;
-   }
-
-    getPassphraseSalt(): string | null {
-    return this.passphraseSalt;
-    }
-
-    setPassphraseSalt(salt:string){
-    this.passphraseSalt = salt;
-    }
-
-    getIsVaultLocal(): boolean {
-      return this.isVaultLocal;
-    }
-
-    setVaultLocal(isLocal:boolean){
-      this.isVaultLocal = isLocal;
-    }
-
-    getLocalVaultService(): LocalVaultV1Service | null {
-      return this.local_vault_service;
-    }
-
-    setLocalVaultService(service:LocalVaultV1Service){
-      this.local_vault_service = service;
-    }
-
-
-
-    getGoogleDriveSync(): boolean | null {
-    return this.googleDriveSync;
-    }
-
-    setGoogleDriveSync(sync:boolean){
-    this.googleDriveSync = sync;
-    }
-
-    getVaultTags(): string[]  {
-      return this.vault_tags;
-    }
-
-    setVaultTags(tags:string[]){
-      this.vault_tags = tags;
-    }
-
 
    clear(){
-    this.id = null;
-    this.email = null;
-    this.zke_key = null;
-    this.derivedKeySalt = null;
-    this.vault = null;
-    this.passphraseSalt = null;
-    this.isVaultLocal = false;
-    this.local_vault_service = null;
-    this.googleDriveSync = null;
-    this.vault_tags = [];
+    this.id.set(null);
+    this.email.set(null);
+    this.zke_key .set(null);
+    this.derivedKeySalt.set(null);
+    this.vault.set(null);
+    this.passphraseSalt.set(null);
+    this.isVaultLocal.set(false);
+    this.local_vault_service.set(null);
+    this.googleDriveSync.set(null);
+    this.vault_tags.set([]);
     localStorage.removeItem("email");
    }
 
    clearVault(){
-    this.vault = null;
-    this.zke_key = null;
-    this.derivedKeySalt = null;
-    this.passphraseSalt = null;
-    this.isVaultLocal = false;
+    this.vault.set(null);
+    this.zke_key.set(null);
+    this.derivedKeySalt.set(null);
+    this.passphraseSalt.set(null);
+    this.isVaultLocal.set(false);
     
    }
 
