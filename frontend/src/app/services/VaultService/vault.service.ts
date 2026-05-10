fffb79ffebc8e620aac7d9313a6353807bf834b5
SeaweedbrainCY <nathanstchepinsky@gmail.com>
2026-05-10 17:40:30 -0400

    feat(frontend): Simplify the vault decryption logic and introduce decryption error reporting
    decryptVault func now returns DecryptedVaultResult interface including error catched by the inner func
    Fix file formatting

--- frontend/src/app/services/VaultService/vault.service.ts
+++ frontend/src/app/services/VaultService/vault.service.ts
@@ -3,17 +3,18 @@ import { Crypto } from '../../common/Crypto/crypto';
 import { TranslateService } from '@ngx-translate/core';
 import { Utils } from '../../common/Utils/utils';
 import { Buffer } from 'buffer';
+import { TOTPEntry, UserService } from '../User/user.service';
 
 
 
-export enum VaultDecryptionStatus {
-  SUCCESS = 0,
-  
+export interface DecryptedVaultResult {
+  vault: Map<string, TOTPEntry>;
+  errors: string[];
 }
 
 
 @Injectable({
-providedIn: 'root'
+  providedIn: 'root'
 })
 export class VaultService {
 
@@ -22,12 +23,13 @@ export class VaultService {
   constructor(
     private crypto: Crypto,
     private translate: TranslateService,
-    private utils: Utils
+    private utils: Utils,
+    private userServive: UserService,
   ) { }
 
-  decryptZKEKey(zke_key_encrypted: string, derivedKey: CryptoKey, isVaultLocal:Boolean=false): Promise<CryptoKey> {
+  decryptZKEKey(zke_key_encrypted: string, derivedKey: CryptoKey, isVaultLocal: Boolean = false): Promise<CryptoKey> {
     return new Promise((resolve, reject) => {
-      this.crypto.decrypt(zke_key_encrypted, derivedKey).then(zke_key_b64=>{
+      this.crypto.decrypt(zke_key_encrypted, derivedKey).then(zke_key_b64 => {
         if (zke_key_b64 != null) {
           const zke_key_raw = Buffer.from(zke_key_b64!, 'base64');
 
@@ -37,34 +39,34 @@ export class VaultService {
             "AES-GCM",
             true,
             ["encrypt", "decrypt"]
-          ).then((zke_key)=>{
+          ).then((zke_key) => {
             resolve(zke_key);
-          }, (error)=>{
-            this.translate.get("login.errors.import_vault.key_dec").subscribe((translation)=>{
-            reject(translation + " " + error);
+          }, (error) => {
+            this.translate.get("login.errors.import_vault.key_dec").subscribe((translation) => {
+              reject(translation + " " + error);
             });
           });;
         } else {
-          if(isVaultLocal){
-            this.translate.get("login.errors.import_vault.wrong_passphrase").subscribe((translation)=>{
+          if (isVaultLocal) {
+            this.translate.get("login.errors.import_vault.wrong_passphrase").subscribe((translation) => {
               reject(translation);
             });
           } else {
-            this.translate.get("login.errors.import_vault.key_dec").subscribe((translation)=>{
-            reject(translation);
+            this.translate.get("login.errors.import_vault.key_dec").subscribe((translation) => {
+              reject(translation);
             });
           }
-          
+
         }
-        
+
       });
-  });
+    });
   }
 
-  derivePassphrase(derivedKeySalt:string, passphrase:string) : Promise<CryptoKey>{
+  derivePassphrase(derivedKeySalt: string, passphrase: string): Promise<CryptoKey> {
     return new Promise((resolve, reject) => {
-      if(derivedKeySalt != null){
-        this.crypto.deriveKey(derivedKeySalt, passphrase).then(key=>{
+      if (derivedKeySalt != null) {
+        this.crypto.deriveKey(derivedKeySalt, passphrase).then(key => {
           resolve(key);
         });
       } else {
@@ -73,48 +75,50 @@ export class VaultService {
     });
   }
 
-  decryptVault(encrypted_vault:Array<Map<string, string>>, zke_key: CryptoKey): Promise<Map<string, Map<string, string>>> {
+  decryptVault(encrypted_vault: Array<Map<string, string>>, zke_key: CryptoKey): Promise<DecryptedVaultResult> {
     return new Promise((resolve, reject) => {
-      let decrypted_vault = new Map<string, Map<string, string>>();
-      const promises: Promise<void>[] = [];
+      let decrypted_vault = new Map<string, TOTPEntry>();
 
+      let errors: string[] = []
+      const fakeProperty: TOTPEntry = {
+        color: "info",
+        name: "Error",
+        secret: "",
+        favicon: false,
+        uri: "",
+        tags: []
+      }
 
-      for (let secret of encrypted_vault){
+      const promises = encrypted_vault.map((secret) => {
         const uuid = secret.get("uuid");
         const enc_secret = secret.get("enc_secret");
-        if(uuid != null){
-          if (enc_secret != null){
-            const promise = this.crypto.decrypt(enc_secret, zke_key).then((dec_secret)=>{
-              if(dec_secret != null){
-                decrypted_vault.set(uuid, this.utils.mapFromJson(dec_secret));
+        if (uuid != null) {
+          if (enc_secret != null) {
+            this.crypto.decrypt(enc_secret, zke_key).then((dec_secret) => {
+              if (dec_secret != null) {
+                decrypted_vault.set(uuid, this.userServive.TOTPEntryFromJSON(dec_secret))
               } else {
-                let fakeProperty = new Map<string, string>();
-                fakeProperty.set("color","info");
-                fakeProperty.set("name", "An error occured while decrypting this secret. Decrypted secret null" );
-                fakeProperty.set("secret", "");
                 decrypted_vault.set(uuid, fakeProperty);
+                errors.push("Decrypted secret null.")
               }
-
-            }, (error)=>{
-              let fakeProperty = new Map<string, string>();
-              fakeProperty.set("color","info");
-              fakeProperty.set("name", "An error occured while decrypting this secret." );
-              fakeProperty.set("secret", "");
+            }, (error) => {
               decrypted_vault.set(uuid, fakeProperty);
+              errors.push(error)
             });
-            promises.push(promise);
           } else {
-            let fakeProperty = new Map<string, string>();
-              fakeProperty.set("color","info");
-              fakeProperty.set("name", "An error occured while decrypting this secret. No secret found.");
-              fakeProperty.set("secret", "");
-              decrypted_vault.set(uuid, fakeProperty);
+            decrypted_vault.set(uuid, fakeProperty);
           }
         }
-      }
+      })
+
       Promise.all(promises)
-      .then(() => resolve(decrypted_vault))
-      .catch((error) => reject(error)); 
+        .then(() => {
+          resolve({
+            vault: decrypted_vault,
+            errors: errors
+          })
+        })
+        .catch((error) => reject(error));
     });
   }
 
