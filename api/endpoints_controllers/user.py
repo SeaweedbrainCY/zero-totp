47125f7849763449085b18a8f8b022922ae0936c
SeaweedbrainCY <nathanstchepinsky@gmail.com>
2026-05-18 17:20:32 -0400

    feat(api): Improve passhrase/vault update mechanism and fail-management
    Instead of a self-managed and partially managed error, the datase is now updated once through 1 unique transaction. The transation is rolledback if any error is detected.
    This avoid issues during the update (network, logic, compute, ....) which would be resulting in a database corruption

--- api/endpoints_controllers/user.py
+++ api/endpoints_controllers/user.py
@@ -1,7 +1,12 @@
 from environment import conf, logging
 from Utils.security_wrapper import require_valid_user
 from database.user_repo import User as UserRepo
-import json
+from database.update_passphrase_repo import UpdatePassphraseRepo
+from database.totp_secret_repo import TOTP_secret as TOTP_secretDB
+from CryptoClasses.hash_func import Bcrypt
+import Utils.utils as utils
+from flask import request, redirect, make_response
+import threading
 
 
 # GET /user/derived-key-salt
@@ -25,15 +30,13 @@ def get_derived_key_salt(user_id):
 #PUT /update/vault 
 @require_valid_user
 def update_vault(user_id, body):
-    returnJson = {"message": "Internal server error", "hashing":-1, "totp":-1, "user":-1, "zke":-1}
     try:
-        newPassphrase = body["new_passphrase"].strip()
-        old_passphrase = body["old_passphrase"].strip()
-        enc_vault = body["enc_vault"].strip()
-        enc_vault = json.loads(enc_vault)
-        zke_key = body["zke_enc"].strip()
-        passphrase_salt = body["passphrase_salt"].strip()
-        derivedKeySalt = body["derived_key_salt"].strip()
+        newPassphrase: str = body["new_passphrase"].strip()
+        old_passphrase:str = body["old_passphrase"].strip()
+        enc_vault: list[dict[str, str]]  = body["enc_vault"]
+        zke_key: str = body["zke_enc"].strip()
+        passphrase_salt: str = body["passphrase_salt"].strip()
+        derivedKeySalt:str = body["derived_key_salt"].strip()
     except Exception as e:
         logging.error(e)
         return '{"message": "Invalid request"}', 400
@@ -43,11 +46,8 @@ def update_vault(user_id, body):
     
     
     
-    is_vault_valid, vault_validation_msg = utils.unsafe_json_vault_validation(enc_vault)
-    if not is_vault_valid:
-        return {"message": vault_validation_msg}, 400
-    userDb = UserDB()
-    zke_db = ZKE_DB()
+   
+    userDb = UserRepo()
     totp_secretDB = TOTP_secretDB()
 
     user = userDb.getById(user_id)
@@ -63,8 +63,7 @@ def update_vault(user_id, body):
         return {"message": "passphrase too long. It must be <70 char"}, 400
     except Exception as e:
         logging.warning("Uknown error occured while hashing password" + str(e))
-        returnJson["hashing"]=0
-        return returnJson, 500
+        return {"message": "Internal Server Error. Code 56af5a20-c8c8-4b30-89ff-a8ae5b57c865."}, 500
 
     logging.info(f"User {user_id} is updating their passphrase. Old passphrase is verified. Proceeding with vault update. IP : {utils.get_ip(request)}")
     
@@ -72,44 +71,33 @@ def update_vault(user_id, body):
     if len(old_vault) != len(enc_vault):
         logging.warning(f"User {user_id} tried to update his vault but the number of secrets in the new vault is different from the old one. The update is rejected.")
         return {"message": "To avoid the loss of your information, Zero-TOTP is rejecting this request because it has detected that you might lose data. Please contact quickly Zero-TOTP developers to fix issue."}, 400
-    for secret in old_vault:
-        if secret.uuid not in enc_vault:
-            logging.warning(f"FORBIDDEN. The user {user_id} tried to update but the secret {secret.uuid} is missing in the new vault. The update is rejected.")
+    
+    uuids_from_old_vault = [secret.uuid for  secret in old_vault]
+    uuids_from_new_vault = [item["uuid"] for  item in enc_vault]
+
+    for item in enc_vault:
+        if item["uuid"] not in uuids_from_old_vault:
+            logging.warning(f"FORBIDDEN. The user {user_id} tried to update but the secret {item["uuid"]} was not in his vault. The update is rejected.")
             return {"message": "Forbidden action. Zero-TOTP detected that you were updating object you don't have access to. The request is rejected."}, 403
+
+
+    for secret in old_vault:
+        if secret.uuid not in uuids_from_new_vault:
+            logging.warning(f"Error. The user {user_id} tried to update but the secret {secret.uuid} was not sent in his new vault. This means the secret would not have been updated. The update is rejected.")
+            return {"message": "Missing parameters. One of the user's secret would not have been updated because missing from the update request. Request aborted."}, 400
+        
     
 
-    returnJson["hashing"]=1
-    errors = 0
-    for secret in enc_vault.keys():
-        totp = totp_secretDB.get_enc_secret_of_user_by_uuid(user_id, secret)
-        if not totp:
-            totp = totp_secretDB.add(user_id=user_id, enc_secret=enc_vault[secret], uuid=secret)
-            if not totp:
-                logging.warning("Unknown error while adding encrypted secret for user " + str(user_id))
-                errors = 1
-        else:
-            if totp.user_id != user_id:
-                logging.warning("User " + str(user_id) + " tried to update secret " + str(secret) + " which is not his")
-                errors = 1
-            else :
-                totp = totp_secretDB.update_secret(uuid=secret, enc_secret=enc_vault[secret], user_id=user_id)
-                if totp == None:
-                    logging.warning("User " + str(user_id) + " tried to update secret " + str(secret) + " but an error occurred server side while storing your  encrypted secret")
-                    errors = 1
-    zke = zke_db.update(user_id, zke_key)
-    userUpdated = userDb.update(user_id=user_id, passphrase=hashedpw, passphrase_salt=passphrase_salt, derivedKeySalt=derivedKeySalt)
-    returnJson["totp"]=1 if errors == 0 else 0
-    returnJson["user"]=1 if userUpdated else 0
-    returnJson["zke"]=1 if zke else 0
-    if errors == 0 and userUpdated and zke:
-        try:
-            ip = utils.get_ip(request)
-            thread = threading.Thread(target=utils.send_information_email,args=(ip, user.mail, "Your vault passphrase has been updated"))
-            thread.start()
-        except Exception as e:
-            logging.error("Unknown error while sending information email" + str(e))
-        logging.info(f"User {user_id} has successfully updated their vault and passphrase.")
-        return {"message": "Vault updated"}, 201
-    else:
-        logging.warning("An error occured while updating passphrase of user " + str(user_id))
-        return returnJson, 500
+    try:
+        UpdatePassphraseRepo().updatePassphrase(user_id=user_id, newPassphrase=hashedpw, newZKEKey=zke_key, newPassphraseSalt=passphrase_salt, newDerivedKeySalt=derivedKeySalt, newVault=enc_vault)
+    except Exception as e:
+        logging.error(f"An error occured while updating the passphrase of user {user_id}. Error: {e}")
+        return {"message": "Internal Server Error. Code 26db0b56-c51e-4515-a4ae-f3e4927f7da2."}, 500
+    try:
+        ip = utils.get_ip(request)
+        thread = threading.Thread(target=utils.send_information_email,args=(ip, user.mail, "Your vault passphrase has been updated"))
+        thread.start()
+    except Exception as e:
+        logging.error("Unknown error while sending information email" + str(e))
+    logging.info(f"User {user_id} has successfully updated their vault and passphrase.")
+    return {"message": "Vault updated"}, 201
