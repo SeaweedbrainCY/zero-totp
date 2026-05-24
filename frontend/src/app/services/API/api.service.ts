fc1ea75081e3cafabb2d6748f8e97be2c38b017e
SeaweedbrainCY <nathanstchepinsky@gmail.com>
2026-05-24 15:44:59 -0400

    feat(frontend): For mobile apps, make the baseURL dependant of the underlying baseURL preferences

--- frontend/src/app/services/API/api.service.ts
+++ frontend/src/app/services/API/api.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
+import { CapacitorPersistentStorageService } from '../Capacitor/persistentStorage/capacitor-persistent-storage.service';
 
 @Injectable({
   providedIn: 'root',
@@ -7,11 +8,25 @@ import { environment } from 'src/environments/environment';
 export class ApiService {
   public baseURL: string;
 
-  constructor() {
-    if (environment.mobile) {
+  constructor(
+    private persistentStorage: CapacitorPersistentStorageService
+  ) {
+    this.baseURL = ""
+    this.updateBaseURL()
+  }
+
+  public async updateBaseURL(): Promise<boolean> {
+    if (environment.isMobileApp) {
       this.baseURL = "https://zero-totp.com"
+      let apiBaseURL = await this.persistentStorage.getAPIBaseURL()
+      if (apiBaseURL != "") {
+        this.baseURL = "https://" + apiBaseURL
+        return true
+      }
+      return false
     } else {
       this.baseURL = "" // Use the global base URL for the webapp
+      return true
     }
   }
 }
