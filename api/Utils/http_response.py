2436dffedf57b4b837cd7c8abe4f1a78c9d65e37
SeaweedbrainCY <nathanstchepinsky@gmail.com>
2026-06-02 16:52:51 -0400

    feat(api): Explicitly set cookies for the base domain only
    This enable the mobile app to request the API even if they are not on the same domain

--- api/Utils/http_response.py
+++ api/Utils/http_response.py
@@ -3,5 +3,5 @@ from environment import conf
 
 class Response(Response):
     def set_auth_cookies(self, session_token, refresh_token):
-        self.set_cookie("session-token", session_token, httponly=True, secure=True, samesite="Lax", max_age=conf.api.refresh_token_validity, path="/api/")
-        self.set_cookie("refresh-token", refresh_token, httponly=True, secure=True, samesite="Lax", max_age=conf.api.refresh_token_validity, path="/api/v1/auth/refresh")
+        self.set_cookie("session-token", session_token, httponly=True, secure=True, samesite="Lax", max_age=conf.api.refresh_token_validity, path="/api/", domain=conf.environment.domain)
+        self.set_cookie("refresh-token", refresh_token, httponly=True, secure=True, samesite="Lax", max_age=conf.api.refresh_token_validity, path="/api/v1/auth/refresh", domain=conf.environment.domain)
