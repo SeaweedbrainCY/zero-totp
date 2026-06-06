724c145278d56ddeebbd406322ddf3a2760b22f6
SeaweedbrainCY <nathanstchepinsky@gmail.com>
2026-06-06 19:00:33 -0400

    feat(frontend): For mobile apps, pre-load and cache auth tokens on application init

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,18 +1,23 @@
 import { Component, OnInit, DOCUMENT } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { Renderer2, Inject } from '@angular/core';
+import { AuthServiceService } from './services/AuthService/auth-service.service';
 
 
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css'],
-    standalone: false
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  standalone: false
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   title = 'frontend';
-  constructor(private renderer: Renderer2, @Inject(DOCUMENT) private document: Document) { 
+  constructor(
+    private renderer: Renderer2,
+    @Inject(DOCUMENT) private document: Document,
+    private authService: AuthServiceService
+  ) {
     const theme = localStorage.getItem('theme');
     if (theme == 'dark') {
       this.renderer.setAttribute(document.documentElement, 'data-theme', 'dark');
@@ -21,9 +26,13 @@ export class AppComponent implements OnInit{
     }
   }
 
-  
+
+
 
   ngOnInit(): void {
+    if (environment.isMobileApp) {
+      this.authService.loadTokenFromKeychain()
+    }
   }
 
 }
