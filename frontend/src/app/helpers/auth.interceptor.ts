8aaf472a13e9e6a9c236e2f40ffd00465849718e
SeaweedbrainCY <nathanstchepinsky@gmail.com>
2026-06-12 22:14:02 -0400

    fix(frontend): For mobiles, properly attach new headers to request after being renewed

--- frontend/src/app/helpers/auth.interceptor.ts
+++ frontend/src/app/helpers/auth.interceptor.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HTTP_INTERCEPTORS, HttpErrorResponse, } from '@angular/common/http';
 
-import { AuthServiceService } from '../services/AuthService/auth-service.service';
+import { AuthServiceService, AuthToken } from '../services/AuthService/auth-service.service';
 import { BehaviorSubject } from 'rxjs';
 
 import { Observable, throwError, filter } from 'rxjs';
 import { catchError, switchMap } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
+import { ApiService } from '../services/API/api.service';
 
 @Injectable()
 export class HttpRequestInterceptor implements HttpInterceptor {
@@ -15,7 +16,8 @@ export class HttpRequestInterceptor implements HttpInterceptor {
 
 
   constructor(
-    private authService: AuthServiceService
+    private authService: AuthServiceService,
+    private apiService: ApiService
   ) { }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
@@ -36,8 +38,18 @@ export class HttpRequestInterceptor implements HttpInterceptor {
     return next.handle(req).pipe(
       catchError((error) => {
         console.log(error);
+
         if (error.status === 403 && error.error.detail === "Session expired") {
-          return this.handleExpiration(req, next) || throwError(() => error);
+          if (environment.isMobileApp) {
+            const configuredAPIURL = new URL(this.apiService.baseURL)
+            const destinationURL = new URL(req.url)
+            if (configuredAPIURL.host == destinationURL.host) {
+              return this.handleExpiration(req, next) || throwError(() => error);
+            }
+          } else {
+            // For webapp the browser won't send the cookies to a foreign domain so it's safe
+            return this.handleExpiration(req, next) || throwError(() => error);
+          }
         }
 
         return throwError(() => error);
@@ -63,7 +75,22 @@ export class HttpRequestInterceptor implements HttpInterceptor {
     if (!this.isRefreshing) {
       this.isRefreshing = true;
       return this.authService.refreshToken().pipe(
-        switchMap(() => {
+        switchMap((response) => {
+          if (environment.isMobileApp) {
+            const apiURL = new URL(this.apiService.baseURL)
+            const tokens: AuthToken = {
+              refresh_token: response.body!.refresh_token ?? "",
+              session_token: response.body!.session_token ?? "",
+              domain: apiURL.host
+            }
+            this.authService.setToken(tokens)
+            const destinationURL = new URL(request.url)
+            if (tokens.domain == destinationURL.host && destinationURL.protocol == "https:") {
+              request = request.clone({
+                setHeaders: { Authorization: `Bearer ${tokens.session_token}` },
+              });
+            }
+          }
           this.isRefreshing = false;
           return next.handle(request);
         }),
@@ -76,6 +103,12 @@ export class HttpRequestInterceptor implements HttpInterceptor {
     } else {
       return this.waitForTokenRefresh().pipe(
         switchMap(() => {
+          const destinationURL = new URL(request.url)
+          if (this.authService.auth_token()?.domain == destinationURL.host && destinationURL.protocol == "https:") {
+            request = request.clone({
+              setHeaders: { Authorization: `Bearer ${this.authService.auth_token()?.session_token}` },
+            });
+          }
           return next.handle(request);
         }), catchError((error) => {
           console.log(error);
